29db9a4b71b485132a57e31bea1d3732e6991935
Chris-Swires <csw@ihtsdo.org>
2015-06-29 09:57:33 +0100

    Endpoint call to check if endpoint 403 error is local config derived, testing on dev-term

--- app/shared/endpoint-service/endpointService.js
+++ app/shared/endpoint-service/endpointService.js
@@ -7,6 +7,11 @@ angular.module('singleConceptAuthoringApp')
                 return $http.get('/config/endpointConfig.json').then(function(response) {
                         return response.data;
                     });
+            },
+            getProjects: function () {
+                return $http.get('/snowowl/ihtsdo-sca/authoring-projects/listProjects').then(function(response) {
+                        return response.data;
+                    });
             }
         };
     }]);
