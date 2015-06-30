4f0ebed7d236a96fa973c4f708c19597544a349f
granvold <pgranvold@westcoastinformatics.com>
2015-06-29 18:20:35 -0700

    WRP-506: Added sca-service calls for UI state WRP-507: Added UI state calls to savedList.js

--- app/shared/savedList/savedList.js
+++ app/shared/savedList/savedList.js
@@ -1,6 +1,29 @@
 'use strict';
 angular.module('singleConceptAuthoringApp.savedList', [])
 
-.controller( 'savedListCtrl', ['$scope', '$location', function savedListCtrl ( $scope, $location) {
- 
-}]);
+  .controller('savedListCtrl', ['$scope', '$location', 'scaService', function savedListCtrl($scope, $location) {
+
+    // name of the panel for the Saved List
+    var panelId = 'saved-list';
+
+    // scope function to save UI state for project, task, and panel
+    // TODO Here just for the sake of wiring, unused
+    $scope.saveUIState = function (projectKey, taskKey, uiState) {
+      scaService.saveUIStateForProjectAndTaskAndPanel(
+        projectKey, taskKey, panelId, uiState)
+        .then(function (uiState) {
+          return uiState;
+        })
+    }
+
+    // scope function to get UI state for project, task, and panel
+    // TODO Here just for the sake of wiring, unused
+    $scope.getUIState = function (projectKey, taskKey, uiState) {
+      scaService.getUIStateForProjectAndTaskAndPanel(
+        projectKey, taskKey, panelId)
+        .then(function (uiState) {
+          return uiState;
+        })
+    }
+
+  }]);
