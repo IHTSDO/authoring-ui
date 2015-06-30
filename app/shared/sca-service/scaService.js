c1c9184078f866e010f93f6d97ffa05ba5c97c71
granvold <pgranvold@westcoastinformatics.com>
2015-06-29 18:20:35 -0700

    WRP-506: Added sca-service calls for UI state WRP-507: Added UI state calls to savedList.js

--- app/shared/sca-service/scaService.js
+++ app/shared/sca-service/scaService.js
@@ -8,6 +8,10 @@ angular.module('singleConceptAuthoringApp')
 
     return {
 
+      /////////////////////////////////////
+      // authoring-projects calls
+      /////////////////////////////////////
+
       // get all projects
       getProjects: function () {
         return $http.get(apiEndpoint + 'projects').then(
@@ -56,12 +60,39 @@ angular.module('singleConceptAuthoringApp')
       // get a specific task for a project
       getTaskForProject: function (projectKey, taskKey) {
         return $http.get(apiEndpoint + 'projects/' + projectKey + '/tasks/' + taskKey).then(
-            function (response) {
-              return response;
-            }, function (error) {
-              // TODO Handle errors
-            }
-          );
+          function (response) {
+            return response;
+          }, function (error) {
+            // TODO Handle errors
+          }
+        );
+      },
+
+      /////////////////////////////////////
+      // ui-state calls
+      /////////////////////////////////////
+
+      // get the UI state for a project, task, and panel triplet
+      getUIState: function (projectKey, taskKey, panelId) {
+        return $http.get(apiEndpoint + 'projects/' + projectKey + '/tasks/' + taskKey + '/panel' + panelId).then(
+          function (response) {
+            return response;
+          }, function (error) {
+            // TODO Handle errors
+          }
+        );
+      },
+
+      // save the UI state for a project, task, and panel triplet
+      saveUIState: function (projectKey, taskKey, panelId, uiState) {
+        return $http.post(apiEndpoint + 'projects/' + projectKey + '/tasks/' + taskKey + '/panel' + panelId, uiState).then(
+          function (response) {
+            return response;
+          }, function (error) {
+            // TODO Handle errors
+          }
+        );
       }
-    };
+    }
+
   }]);
