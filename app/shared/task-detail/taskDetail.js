b55bda30bc87d9466b08c84798e292fe399a8ded
granvold <pgranvold@westcoastinformatics.com>
2015-07-23 15:05:07 -0700

    WRP-715:  Further work on classification result tabs, relationships are now separated by type WRP-709:  Task Detail and Classification components wired to receive classification broadcasts WRP-657:  Initial polling setup

--- app/shared/task-detail/taskDetail.js
+++ app/shared/task-detail/taskDetail.js
@@ -1,10 +1,16 @@
 'use strict';
 angular.module('singleConceptAuthoringApp.taskDetail', [])
 
-  .controller('taskDetailCtrl', ['$scope', '$routeParams', '$location', 'scaService', 'snowowlService', function taskDetailCtrl($scope, $routeParams, $location, scaService) {
+  .controller('taskDetailCtrl', ['$rootScope', '$scope', '$routeParams', '$location', 'scaService', 'snowowlService', function taskDetailCtrl($rootScope, $scope, $routeParams, $location, scaService) {
 
     var panelId = 'task-detail';
     $scope.task = null;
+
+    $rootScope.$on('setClassification', function setClassification(event, data) {
+
+      console.debug('task.js received setClassification event', data);
+
+    });
 
     // scope function to save UI state for project, task, and panel
     // TODO Here just for the sake of wiring, unused
@@ -17,8 +23,12 @@ angular.module('singleConceptAuthoringApp.taskDetail', [])
     };
 
     $scope.classify = function() {
-      scaService.startClassification($routeParams.projectId, $routeParams.taskId);
+      scaService.startClassification($routeParams.projectId, $routeParams.taskId).then(function(response) {
+        $rootScope.$broadcast('startClassification', {result : response});
+      })
     };
+
+
 
     function initialize() {
       scaService.getTaskForProject($routeParams.projectId, $routeParams.taskId).then(function (response) {
