8742703531a70a2fd6e60a53e3f5e58f75b33089
granvold <pgranvold@westcoastinformatics.com>
2015-08-17 18:17:31 -0700

    WRP-781:  Concepts in Review list can be reodered WRP-904:  Marking task "in review" or "review complete" WRP-929:  Further page framework improvements to Feedback view Fixed bad boolean logic resulting in hidden buttons on classification.html Changed incorrect titles on View buttons in edit.html. Some formatting and code cleanup.

--- app/shared/feedback/feedback.js
+++ app/shared/feedback/feedback.js
@@ -2,15 +2,16 @@
 
 angular.module('singleConceptAuthoringApp')
 
-  .directive('feedback', ['$rootScope', 'ngTableParams', '$routeParams', '$timeout',
-    function ($rootScope, NgTableParams, $routeParams) {
+  .directive('feedback', ['$rootScope', 'ngTableParams', '$routeParams', '$filter', '$timeout',
+    function ($rootScope, NgTableParams, $routeParams, $filter, $timeout) {
       return {
         restrict: 'A',
         transclude: false,
         replace: true,
         scope: {
           // feedback container structure:
-          // { report: [feedback report], ...}
+          // { conceptsToReview: [...], conceptsReviewed: [...] }
+
           feedbackContainer: '=',
 
           // flag for whether or not to allow editing controls
@@ -29,20 +30,112 @@ angular.module('singleConceptAuthoringApp')
 
           // instantiate validation container if not supplied
           if (!scope.feedbackContainer) {
-            scope.feedbackContainer = {concepts: []};
-          }
-            
-          //demo data
-          var concepts = [
-              {fsn : 'Concept 1 (demo)', conceptId : 1092837},
-              {fsn : 'Concept 2 (demo)', conceptId : 1092837},
-              {fsn : 'Concept 3 (demo)', conceptId : 1092837},
-              {fsn : 'Concept 4 (demo)', conceptId : 1092837}
-          ];
+            scope.feedbackContainer = {
+              conceptsToReview: [
+                {
+                  fsn: 'Concept 1 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 2 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 3 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 4 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 5 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 6 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 7 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 8 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                }
+              ],
+              conceptsReviewed: [
+                {
+                  fsn: 'Concept 1 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 2 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 3 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                },
+                {
+                  fsn: 'Concept 4 (demo)',
+                  conceptId: 1092837,
+                  feedback: 'Some sample feedback'
+                }
+              ]
+            }
+          };
 
+
+          // arrays used for ng-table data
+          scope.conceptsToReview = [];
+          scope.conceptsReviewed = [];
 
           // declare table parameters
-          scope.feedbackTableParams = new NgTableParams({
+          scope.conceptsToReviewTableParams = new NgTableParams({
+              page: 1,
+              count: 10,
+              sorting: {fsn: 'asc'},
+              orderBy: ''
+            },
+            {
+              filterDelay: 50,
+              total: scope.feedbackContainer && scope.feedbackContainer.conceptsToReview ?
+                scope.feedbackContainer.conceptsToReview.length : 0,
+
+              getData: function ($defer, params) {
+
+                console.debug('getData for conceptsToReview from ', scope.feedbackContainer.conceptsToReview)
+
+                if (!scope.feedbackContainer.conceptsToReview || scope.feedbackContainer.conceptsToReview.length === 0) {
+                  $defer.resolve([]);
+                } else {
+
+                  var orderedData = scope.feedbackContainer.conceptsToReview;
+
+                  params.total(scope.feedbackContainer.conceptsToReview.length);
+                  orderedData = params.sorting() ? $filter('orderBy')(orderedData, params.orderBy()) : orderedData;
+
+                  scope.conceptsToReview = (orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                }
+              }
+            }
+          );
+
+          // declare table parameters
+          scope.conceptsReviewedTableParams = new NgTableParams({
               page: 1,
               count: 10,
               sorting: {fsn: 'desc'},
@@ -50,36 +143,65 @@ angular.module('singleConceptAuthoringApp')
             },
             {
               filterDelay: 50,
-              total: concepts.length,
+              total: scope.feedbackContainer && scope.feedbackContainer.conceptsReviewed ?
+                scope.feedbackContainer.conceptsReviewed.length : 0,
               getData: function ($defer, params) {
 
-                if (!concepts || concepts.length === 0) {
+                console.debug('getData for conceptsReviewed from ', scope.feedbackContainer.conceptsReviewed)
+
+                if (!scope.feedbackContainer.conceptsReviewed || scope.feedbackContainer.conceptsReviewed.length === 0) {
                   $defer.resolve([]);
                 } else {
 
-                  var orderedData = concepts;
+                  var orderedData = scope.feedbackContainer.conceptsReviewed;
 
-                  params.total(concepts.length);
+                  params.total(scope.feedbackContainer.conceptsReviewed.length);
                   orderedData = params.sorting() ? $filter('orderBy')(orderedData, params.orderBy()) : orderedData;
 
-                  $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                  scope.conceptsReviewed = (orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
                 }
               }
             }
           );
 
-          // watch for changes in the validation in order to populate tables
-          scope.$watch('feedbackContainer', function () {
+          // function called when dropping concept
+          // targetIndex: the point at which to insert the dropped concept
+          // conceptObj: {startIndex: N, concept: {...}}
+          scope.dropConceptToReview = function (targetIndex, conceptObj) {
+            console.debug('drop event', targetIndex, conceptObj);
 
-            if (!scope.feedbackContainer) {
-              return;
+            if (targetIndex === conceptObj.index) {
+              // do nothing
+            } else {
+
+
+
+              // disable auto-sorting of table params  -- otherwise drag/drop
+              // makes no sense!
+              scope.conceptsToReviewTableParams.sorting('');
+
+              // insert very slight timeout to allow dragndrop to check for requested
+              // effects (deleting too fast causes undefined-access errors)
+              $timeout(function () {
+                // remove the passed object
+                scope.feedbackContainer.conceptsToReview.splice(conceptObj.index, 1);
+
+               // insert the dragged concept at the target index
+                scope.feedbackContainer.conceptsToReview.splice(targetIndex, 0, conceptObj.concept);
+
+                scope.conceptsToReviewTableParams.reload();
+
+              }, 10);
+
             }
-            // reload the tables
-            scope.feedbackTableParams.reload();
 
-          }, true); // make sure to check object inequality, not reference!
+          };
 
-          
+
+          // on load, initialize tables
+          scope.conceptsToReviewTableParams.reload();
+          scope.conceptsReviewedTableParams.reload();
+
         }
 
       };
