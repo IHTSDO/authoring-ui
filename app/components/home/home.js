ab7f567af708ee900881d142731c041509bc871b
granvold <pgranvold@westcoastinformatics.com>
2015-08-24 12:18:35 -0700

    Fixed task and review task display functionality problems WRP-991: Allow input of role group on relationship creation

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -14,7 +14,7 @@ angular.module('singleConceptAuthoringApp.home', [
       });
   })
 
-  .controller('HomeCtrl', function HomeCtrl($scope, $rootScope, $timeout, ngTableParams, $filter, $modal, $location, scaService, snowowlService) {
+  .controller('HomeCtrl', function HomeCtrl($scope, $rootScope, $timeout, ngTableParams, $filter, $modal, $location, scaService, snowowlService, notificationService) {
 
     // TODO Placeholder, as we only have the one tab at the moment
     $rootScope.pageTitle = "My Tasks";
@@ -26,7 +26,7 @@ angular.module('singleConceptAuthoringApp.home', [
     $scope.tableParams = new ngTableParams({
         page: 1,
         count: 10,
-        sorting: {name: 'asc'}
+        sorting: {updated: 'desc', name: 'asc'}
       },
       {
         filterDelay: 50,
@@ -63,7 +63,7 @@ angular.module('singleConceptAuthoringApp.home', [
     $scope.reviewTableParams = new ngTableParams({
         page: 1,
         count: 10,
-        sorting: {name: 'asc'}
+        sorting: {updated: 'desc', name: 'asc'}
       },
       {
         filterDelay: 50,
@@ -97,31 +97,60 @@ angular.module('singleConceptAuthoringApp.home', [
       }
     );
 
-    // watch for task creation events
-    $scope.$on('taskCreated', function (event, task) {
-      if ($scope.tasks) {
-        $scope.tasks.unshift(task);
-      }
-    });
+    // TODO Workaround to capture full review functionality
+    // Replace with loadAllTasks when endpoints are complete
+    function loadWRPASTasks() {
+
+      notificationService.sendMessage('Loading tasks...', 0);
+
+      $scope.tasks = [];
+      $scope.reviewTasks = [];
+
+      var tasks = [];
+      var reviewTasks = [];
+
+      // get tasks from WRPAS project
+      // TODO Change this once we have API call for review-tasks
+      scaService.getTasksForProject('WRPAS').then(function (response) {
+        console.debug('Retrieved tasks for project WRPAS', response);
+        angular.forEach(response, function (task) {
+
+      //    console.debug('Checking task', task.key, task.status, task.reviewer);
+
+          // add all ready for review tasks
+          if (task.status === 'In Review' && !task.reviewer) {
+            reviewTasks.push(task);
+          }
+
+          // add all assigned tasks in review that match the logged in user
+          // into My Tasks.
+          else if (task.reviewer && task.reviewer.username === $rootScope.accountDetails.login) {
+            tasks.push(task);
+          }
+
+          // otherwise, push into tasks
+          else if (task.assignee && task.assignee.username === $rootScope.accountDetails.login) {
+            tasks.push(task);
+          }
+        });
+
+        $scope.tasks = tasks;
+        $scope.reviewTasks = reviewTasks;
+
+        notificationService.sendMessage('All tasks loaded', 5000);
+
+      });
+    }
 
     // on successful set, reload table parameters
     $scope.$watch('tasks', function () {
-      if (!$scope.tasks || $scope.tasks.length == 0) {
-      }
-      else {
-        $scope.tableParams.reload();
-      }
+      $scope.tableParams.reload();
 
     }, true);
 
     // on successful set, reload table parameters
     $scope.$watch('reviewTasks', function () {
-      if (!$scope.reviewTasks || $scope.reviewTasks.length == 0) {
-      }
-      else {
-        $scope.reviewTableParams.reload();
-      }
-
+      $scope.reviewTableParams.reload();
     }, true);
 
     $scope.openCreateTaskModal = function () {
@@ -130,11 +159,16 @@ angular.module('singleConceptAuthoringApp.home', [
         controller: 'taskCtrl'
       });
 
-      modalInstance.result.then(function () {
+      modalInstance.result.then(function (response) {
+        console.debug('modal closed with response', response);
+        if (response) {
+
+          // TODO CHange to loadAllTasks once endpoints are complete
+          loadWRPASTasks();
+        }
       }, function () {
       });
     };
-
 
     $scope.assignReviewer = function (task) {
       if (task && !task.reviewer) {
@@ -147,21 +181,22 @@ angular.module('singleConceptAuthoringApp.home', [
           }
         };
 
-        scaService.updateTask(task.projectKey, task.key, updateObj).then(function() {
+        scaService.updateTask(task.projectKey, task.key, updateObj).then(function () {
           $location.url('feedback/' + task.projectKey + '/' + task.key);
         })
       } else {
         notificationService.sendError("Cannot claim review task, already assigned", 10000, null, null);
       }
     }
-    $scope.isReviewer = function(){
-        return accountService.isReviewer();  
+    $scope.isReviewer = function () {
+      return accountService.isReviewer();
     };
 
 // Initialization:  get tasks and classifications
     function initialize() {
       $scope.tasks = [];
       $scope.reviewTasks = [];
+
       // get all projects for task creation
       scaService.getProjects().then(function (response) {
         if (!response || response.length == 0) {
@@ -169,56 +204,41 @@ angular.module('singleConceptAuthoringApp.home', [
           return;
         } else {
           $scope.projects = response;
-
-          // get tasks from WRPAS project
-          // TODO Change this once we have API call for review-tasks
-
-          scaService.getTasksForProject('WRPAS').then(function (response) {
-            console.debug('Retrieved tasks for project WRPAS', response);
-            angular.forEach(response, function (task) {
-
-              console.debug('Checking task', task.key, task.status, task.reviewer);
-
-              // add all ready for review tasks
-              if (task.status === 'In Review' && !task.reviewer) {
-                $scope.reviewTasks.push(task);
-              }
-
-              // add all assigned tasks in review that match the logged in user
-              // into My Tasks.
-              else if (task.status === 'In Review' || task.status === 'Review Complete' && task.reviewer && task.reviewer.username === $rootScope.accountDetails.login) {
-                $scope.tasks.push(task);
-              }
-            });
-          })
         }
       });
-/*
-      Commented out for now -- tasks are sorted above
 
-      // get tasks across all projects
-      $scope.tasks = [];
-      scaService.getTasks().then(function (response) {
-        if (!response || response.length == 0) {
-          $scope.tasks = [];
-          return;
-        }
+      // temporary workaround, restricting to WRPAS tasks
+      // and getting
+      loadWRPASTasks();
 
-        $scope.tasks = response;
-      }, function (error) {
-      });*/
+      /*
+       // TODO Commented out until endpoints are fleshed out for review tasks
+       // get tasks across all projects
+       $scope.tasks = [];
+       scaService.getTasks().then(function (response) {
+       if (!response || response.length == 0) {
+       $scope.tasks = [];
+       return;
+       }
 
-      $timeout(function () {
-        scaService.getTasks().then(function (response) {
-          if (!response || response.length == 0) {
-            $scope.tasks = [];
-            return;
-          }
+       $scope.tasks = response;
+       }, function (error) {
+       });
+       */
+      /*
+       // disable polling
+       $timeout(function () {
+       scaService.getTasks().then(function (response) {
+       if (!response || response.length == 0) {
+       $scope.tasks = [];
+       return;
+       }
 
-          $scope.tasks = response;
-        }, function (error) {
-        });
-      }, 30000);
+       $scope.tasks = response;
+       }, function (error) {
+       });
+       }, 30000);
+       */
 
     }
 
