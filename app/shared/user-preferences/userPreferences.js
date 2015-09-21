f8a36c841bf2685d3c33dae221aa9439b5002dd2
granvold <pgranvold@westcoastinformatics.com>
2015-09-21 12:57:44 -0700

    Additional message/error handling for user preferences

--- app/shared/user-preferences/userPreferences.js
+++ app/shared/user-preferences/userPreferences.js
@@ -7,24 +7,33 @@ angular.module('singleConceptAuthoringApp.taskDetail', [])
     $scope.userPreferences = accountService.getUserPreferences();
 
     // revert settings to original
-    $scope.revert = function() {
+    $scope.revert = function () {
       $scope.userPreferences = accountService.getUserPreferences();
     };
 
     // close modal and apply settings
-    $scope.save = function() {
-      accountService.setUserPreferences($scope.userPreferences).then(function(userPreferences) {
+    $scope.save = function () {
+      accountService.setUserPreferences($scope.userPreferences).then(function (userPreferences) {
+
         $modalInstance.close(userPreferences);
-      }, function(error) {
-        $scope.errorMsg = 'Unexpected error saving user preferences';
+
+
+        // TODO Enable once UI States are available
+       /* scaService.saveUIState('user-preferences').then(
+          function (response) {
+            notificationService.sendMessage('Application preferences updated');
+        }, function (error) {
+            notificationService.sendError('Unexpected error saving settings. Your changes may not persist across sessions');
+        });*/
+
+      }, function (error) {
+        $scope.errorMsg = 'Unexpected error applying settings';
       })
 
     };
 
-    $scope.cancel = function() {
+    $scope.cancel = function () {
       $modalInstance.cancel();
     }
-
-
 
   }]);
