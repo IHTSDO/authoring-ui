5f1117df1e5ae263f0e678c7f1cfcba6b9a9ca8f
granvold <pgranvold@westcoastinformatics.com>
2015-10-01 16:54:51 -0700

    WRP-986: Editing existing tasks WRP-632: Temporarily removed html elements pending input from Ashley

--- app/shared/task/task.js
+++ app/shared/task/task.js
@@ -34,10 +34,17 @@ angular.module('singleConceptAuthoringApp')
             }
         };
     })
-  .controller('taskCtrl', ['$scope', '$rootScope', '$modalInstance', 'scaService', function ($scope, $rootScope, $modalInstance, scaService) {
+  .controller('taskCtrl', function ($scope, $rootScope, $modalInstance, scaService, task) {
 
+    console.debug('task.js with task', task)
     // scope variables
     $scope.projects = null;
+    $scope.task = task;
+
+    // if no task passed in, create empty object
+    if (!$scope.task) {
+      $scope.task = {};
+    }
 
     ///////////////////////////////////////////
 
@@ -62,50 +69,64 @@ angular.module('singleConceptAuthoringApp')
     initialize();
 
     // closes the modal instance (if applicable)
-    $scope.close = function() {
+    $scope.close = function () {
       $modalInstance.close();
     };
 
     // Creates a task from modal form
-    $scope.createTask = function (taskTitle, taskProject, taskDetails) {
+    $scope.createTask = function () {
 
       // check that all required fields are present
-      if (!taskTitle) {
+      if (!$scope.task.summary) {
         window.alert('You must specify a title');
         return;
       }
-      if (!taskProject) {
+      if (!$scope.task.projectKey) {
         window.alert('You must specify a project');
         return;
       }
-      if (!taskDetails) {
-        window.alert('You must specify task details');
-        return;
-      }
-
-      // create JSON object for task creation and call API
-      var authoringTaskCreateRequest = {
-        'summary': taskTitle,
-        'description': taskDetails
-      };
 
       $scope.msgSuccess = 'Creating task...';
       $scope.disabled = true;
-      scaService.createTaskForProject(taskProject.key, authoringTaskCreateRequest).then(
+      scaService.createTaskForProject($scope.task.projectKey, $scope.task).then(
         function (response) {
 
           // close modal
           $modalInstance.close(response);
-      }, function (error) {
+        }, function (error) {
           $scope.disabled = false;
           $scope.msgSuccess = '';
           $scope.msgError = 'Error occurred when trying to create task: ' + error;
-      });
+        });
 
     };
 
-    $scope.clearMessages = function() {
+    $scope.updateTask = function () {
+
+      console.debug('updateTask', $scope.task);
+
+      // check that all required fields are present
+      if (!$scope.task.summary) {
+        window.alert('You must specify a title');
+        return;
+      }
+      if (!$scope.task.assignee) {
+        window.alert('You must specify an assigned user');
+      }
+
+      $scope.msgSuccess = 'Updating task...';
+      $scope.disabled = true;
+      scaService.updateTask($scope.task.projectKey, $scope.task.key, $scope.task).then(function (response) {
+        $modalInstance.close(response);
+      }, function (error) {
+        $scope.disabled = false;
+        $scope.msgSuccess = '';
+        $scope.msgError = 'Error occurred when trying to update task: ' + error;
+      });
+    };
+
+    $scope.clearMessages = function () {
       $scope.msgError = '';
       $scope.msgSuccess = '';
     };
-  }]);
+  });
