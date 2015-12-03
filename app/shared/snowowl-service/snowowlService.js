2255b04a76b32be2dd587bba6bbb54393f47a69b
Chris-Swires <csw@ihtsdo.org>
2015-12-03 12:26:35 +0000

    Fix for fsn expansion on MRCM endpoints

--- app/shared/snowowl-service/snowowlService.js
+++ app/shared/snowowl-service/snowowlService.js
@@ -98,7 +98,7 @@ angular.module('singleConceptAuthoringApp')
       if (limit > 200) {
         limit = 200;
       }
-      $http.get(apiEndpoint + branch + '/concepts/' + conceptId + '/descendants?expand=fsn&' + (offset === -1 ? '' : '&offset=' + offset) + (limit === -1 ? '' : '&limit=' + limit) + '&direct=false').then(function (response) {
+      $http.get(apiEndpoint + branch + '/concepts/' + conceptId + '/descendants?expand=fsn()&' + (offset === -1 ? '' : '&offset=' + offset) + (limit === -1 ? '' : '&limit=' + limit) + '&direct=false').then(function (response) {
         deferred.resolve(response.data);
       }).then(function (error) {
         deferred.reject(error);
@@ -779,7 +779,7 @@ angular.module('singleConceptAuthoringApp')
     //////////////////////////////////////////////////
 
     function getDomainAttributes(branch, parentIds) {
-      return $http.get(apiEndpoint + '/mrcm/' + branch + '/domain-attributes?parentIds=' + parentIds + '&expand=fsn&offset=0&limit=50').then(function (response) {
+      return $http.get(apiEndpoint + '/mrcm/' + branch + '/domain-attributes?parentIds=' + parentIds + '&expand=fsn()&offset=0&limit=50').then(function (response) {
         return response.data ? response.data : [];
       }, function (error) {
         return null;
@@ -787,7 +787,7 @@ angular.module('singleConceptAuthoringApp')
     }
 
     function getAttributeValues(branch, attributeId, searchStr) {
-      return $http.get(apiEndpoint + '/mrcm/' + branch + '/attribute-values/' + attributeId + '?' + (searchStr ? 'termPrefix=' + encodeURIComponent(searchStr) + (!isNaN(parseFloat(searchStr) && isFinite(searchStr)) ? '' : '*') : '') + '&expand=fsn&offset=0&limit=50').then(function (response) {
+      return $http.get(apiEndpoint + '/mrcm/' + branch + '/attribute-values/' + attributeId + '?' + (searchStr ? 'termPrefix=' + encodeURIComponent(searchStr) + (!isNaN(parseFloat(searchStr) && isFinite(searchStr)) ? '' : '*') : '') + '&expand=fsn()&offset=0&limit=50').then(function (response) {
         return response.data.items ? response.data.items : [];
       }, function (error) {
         return null;
