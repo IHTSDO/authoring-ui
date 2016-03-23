275a22af08c52f3b2dbb97c27816c6f5f1d7645b
Chris-Swires <csw@ihtsdo.org>
2016-03-23 10:28:44 +0000

    WRP-2442

--- app/shared/promotion-service/promotionService.js
+++ app/shared/promotion-service/promotionService.js
@@ -14,7 +14,7 @@ angular.module('singleConceptAuthoringApp')
      *   task being promoted
      * @returns {*|promise} a list of eligibility errors or warnings
      */
-    function checkClassificationPrerequisites(branch, latestClassificationJson) {
+    function checkClassificationPrerequisites(branch, latestClassificationJson, project, task) {
 
       console.log('Checking Promotion requirements', branch, latestClassificationJson);
       var deferred = $q.defer();
@@ -176,10 +176,46 @@ angular.module('singleConceptAuthoringApp')
                     blocksPromotion: true
                   });
                 }
+                scaService.getTaskForProject(project, task).then(function (branchStatus) {
+                    console.log(branchStatus);
+                    ////////////////////////////////////////////////////////////
+                    // CHECK:  Has the Task been reviewed?
+                    ////////////////////////////////////////////////////////////
+                    if (branchStatus.feedbackMessagesStatus === 'none') {
+                      flags.push({
+                        checkTitle: 'No feedback on concepts',
+                        checkWarning: 'No feedback has been left against edited concepts, has this task been reviewed?',
+                        blocksPromotion: false
+                      });
+                    }
 
-                console.debug('resolving');
+                    ////////////////////////////////////////////////////////////
+                    // CHECK:  Is the task still in Review?
+                    ////////////////////////////////////////////////////////////
+                    if (branchStatus.status === 'In Review') {
+                      flags.push({
+                        checkTitle: 'Task is still in Review',
+                        checkWarning: 'The task Review has not been marked as complete.',
+                        blocksPromotion: false
+                      });
+                    }
 
-                deferred.resolve(flags);
+                    ////////////////////////////////////////////////////////////
+                    // CHECK:  Unread Review messages?
+                    ////////////////////////////////////////////////////////////
+                    if (branchStatus.feedbackMessagesStatus === 'unread') {
+                      flags.push({
+                        checkTitle: 'Review contains unread feedback',
+                        checkWarning: 'All feedback against concepts within this task has not been read',
+                        blocksPromotion: false
+                      });
+                    }
+                    console.debug('resolving');
+
+                    deferred.resolve(flags);
+                });
+
+                
               },
               function (error) {
                 deferred.reject('Could not determine branch state');
@@ -215,7 +251,7 @@ angular.module('singleConceptAuthoringApp')
           }]);
         }
 
-        checkClassificationPrerequisites(branch, task.latestClassificationJson).then(function (flags) {
+        checkClassificationPrerequisites(branch, task.latestClassificationJson, projectKey, taskKey).then(function (flags) {
           deferred.resolve(flags);
         }, function (error) {
           deferred.reject(error);
@@ -252,7 +288,7 @@ angular.module('singleConceptAuthoringApp')
           return;
         }
 
-        checkClassificationPrerequisites(branch, project.latestClassificationJson).then(function (flags) {
+        checkClassificationPrerequisites(branch, project.latestClassificationJson, projectKey, taskKey).then(function (flags) {
           deferred.resolve(flags);
         }, function (error) {
           deferred.reject(error);
