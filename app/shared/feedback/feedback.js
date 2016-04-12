b279768e615f560ac20b9462c375347e7003da7f
Chris-Swires <csw@ihtsdo.org>
2016-04-12 08:55:06 +0100

    WRP-2578

--- app/shared/feedback/feedback.js
+++ app/shared/feedback/feedback.js
@@ -478,10 +478,11 @@ angular.module('singleConceptAuthoringApp')
                 break;
               }
             }
-              console.log(data.concept);
+             
             // mark as unviewed in ToReview list (if present)
             angular.forEach(scope.conceptsToReviewViewed, function (item) {
               if (item.conceptId === data.concept.conceptId) {
+                scope.addToEdit(item);
                 item.viewed = false;
               }
             });
@@ -489,14 +490,14 @@ angular.module('singleConceptAuthoringApp')
             // mark as unviewed in Reviewed list (if present)
             angular.forEach(scope.conceptsReviewedViewed, function (item) {
               if (item.conceptId === data.concept.conceptId) {
-                scope.addToEdit(item.conceptId);
+                scope.addToEdit(item);
                 item.viewed = false;
               }
             });
             angular.forEach(scope.conceptsClassified, function (item) {
                 console.log(item);
               if (item.conceptId === data.concept.conceptId) {
-                scope.addToEdit(item.conceptId);
+                scope.addToEdit(item);
                 item.viewed = false;
               }
             });
