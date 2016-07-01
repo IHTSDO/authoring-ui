4ba2e3c5d75cefb1662ace7b8862319087431226
Patrick Granvold <granvold@gmail.com>
2016-07-01 13:26:29 -0700

    WRP-2808: Relationship fields respect locked modules, disabled adding relationships where module is locked

--- app/shared/concept-edit/conceptEdit.js
+++ app/shared/concept-edit/conceptEdit.js
@@ -2173,7 +2173,7 @@ angular.module('singleConceptAuthoringApp').directive('conceptEdit', function ($
 
           // if this is a new TEXT_DEFINITION, apply defaults
           // sensitivity is correctly set
-          if (description.type === 'TEXT_DEFINITION') {
+          if (!description.effectiveTime && description.type === 'TEXT_DEFINITION') {
             description.acceptabilityMap['900000000000509007'] = 'PREFERRED';
             description.acceptabilityMap['900000000000508004'] = 'PREFERRED';
             description.caseSignificance = 'ENTIRE_TERM_CASE_SENSITIVE';
