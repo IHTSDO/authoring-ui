7aedc8a9a9d5855c97c814da49c7cff7df4ea603
Chris-Swires <csw@ihtsdo.org>
2016-07-14 11:01:24 +0100

    WRP-2244

--- app/shared/config-service/configService.js
+++ app/shared/config-service/configService.js
@@ -9,12 +9,12 @@ angular.module('singleConceptAuthoringApp')
         });
       },
       getExcludedValidationRuleIds : function() {
-        return $http.get('/config/validationConfig.json').then(function(response) {
+        return $http.get('/validationConfig/validationConfig.json').then(function(response) {
           return response.data.excludedRuleIds;
         })
       },
       getWhiteListEligibleRuleIds : function() {
-        return $http.get('/config/validationConfig.json').then(function(response) {
+        return $http.get('/validationConfig/validationConfig.json').then(function(response) {
           return response.data.whitelistEligibleIds;
         })
       }
