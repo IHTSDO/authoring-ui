f3f57cb82baa5685457efe3e4e2e86e226ac20b7
Parick Granvold <pgranvold@westcoastinformatics.com>
2016-08-05 15:03:24 -0700

    WRP-2937: concept list and service implementations

--- app/shared/crs-service/crsService.js
+++ app/shared/crs-service/crsService.js
@@ -6,108 +6,97 @@ angular.module('singleConceptAuthoringApp')
  */
   .factory('crsService', function ($http, $rootScope, $q, scaService, snowowlService) {
 
-    var currentTask;
+      var currentTask;
 
-    var currentTaskConcepts = null;
+      var currentTaskConcepts = null;
 
-    function getNewCrsConcept(conceptJson, guid) {
-      return {
-        // the id fields
-        guid: guid,
-        conceptId: conceptJson.conceptId,
-        fsn: conceptJson.fsn,
-        preferredSynonym: conceptJson.preferredSynonym,
+      function getNewCrsConcept(conceptJson, guid) {
+        return {
+          // the id fields
+          guid: guid,
+          conceptId: conceptJson.conceptId,
+          fsn: conceptJson.fsn,
+          preferredSynonym: conceptJson.preferredSynonym,
 
-        // the original concept Json from the CRT ticket
-        conceptJson: conceptJson,
+          // the original concept Json from the CRT ticket
+          conceptJson: conceptJson,
 
-        // flags
-        saved: false,
-        finished: false
+          // flags
+          saved: false,
+          finished: false
 
+        };
       }
-    }
 
-    function getJsonAttachment(url) {
-      var deferred = $q.defer();
-      $http.get(url).then(function (response) {
-        deferred.resolve(response.data);
-      }, function (error) {
-        deferred.reject(error.message);
-      });
-      return deferred.promise;
-    }
-
-    //
-    // Stores the CRS Concept list in two UI-States
-    // - modified-list : used for initial display to author, will be modified
-    // - crs-concepts-list : used to store the list of concepts, unchanging
-    //
-    function storeCrsConcepts(concepts) {
-      scaService.getUiStateForTask(currentTask.projectKey, task.key, 'crs-concepts').then(function (existingList) {
-        var newList = existingList ? existingList : [];
-        angular.forEach(containers, function (container) {
-          if (newList.indexOf(container.guid) === -1) {
-            newList.push(container);
-          }
+      function getJsonAttachment(url) {
+        var deferred = $q.defer();
+        $http.get(url).then(function (response) {
+          deferred.resolve(response.data);
+        }, function (error) {
+          deferred.reject(error.message);
         });
-        scaService.saveUiStateForTask(task.projectKey, task.key, 'crs-concepts', newList);
-      })
-    }
+        return deferred.promise;
+      }
 
-    function initializeFromTask(task) {
-      var deferred = $q.defer();
+      // Stores the CRS Concept list
+      function storeCrsConcepts(concepts) {
+        scaService.saveUiStateForTask(task.projectKey, task.key, 'crs-concepts', concepts);
+      }
 
-      // the list of containers to storea nd return
-      var containers = [];
+      // initialize ui states from a task
+      function initializeFromTask(task) {
+        var deferred = $q.defer();
 
-      // cycle over each linked issue attachment
-      angular.forEach(task.issueLinkAttachments, function (url) {
+        // the list of containers to storea nd return
+        var containers = [];
 
-        var promise = $q.defer();
-        containers.push(promise);
+        // cycle over each linked issue attachment
+        angular.forEach(task.issueLinkAttachments, function (url) {
 
-        // retrieve the JSON attachments
-        getJsonAttachment(url).then(function (jsonAttachment) {
+          var promise = $q.defer();
+          containers.push(promise);
 
-          // cycle over each concept in the attachment
-          angular.forEach(jsonAttachment, function (concept) {
+          // retrieve the JSON attachments
+          getJsonAttachment(url).then(function (jsonConcepts) {
 
-            // create a GUID for this concept
-            var guid = snowowlService.createGuid();
+            // cycle over each concept in the attachment
+            angular.forEach(jsonConcepts, function (jsonConcept) {
 
-            // store the concept against its guid
-            var crsContainer = storeCrsConcept(concept, guid);
+              // create a GUID for this concept
+              var guid = snowowlService.createGuid();
 
-            // add the container to the list
-            promise.resolve(crsContainer);
+              // store the concept against its guid
+              var crsContainer = storeCrsConcept(jsonConcept, guid);
+
+              // add the container to the list
+              promise.resolve(crsContainer);
+            });
           });
-        })
-      });
+        });
 
-      // after all promises complete, store and return the container list
-      $q.all(containers, function () {
-        storeCrsConcepts(containers);
-        deferred.resolve(containers);
-      }, function (error) {
-        deferred.reject(error)
-      });
+        // after all promises complete, store and return the container list
+        $q.all(containers, function () {
+          storeCrsConcepts(containers);
+          deferred.resolve(containers);
+        }, function (error) {
+          deferred.reject(error);
+        });
 
-      return deferred.promise;
-    }
+        return deferred.promise;
+      }
 
-    //
-    // Clear the CRS Concept list for a task
-    //
-    function clearCrsConceptsForTask(task) {
-      var deferred = $q.defer();
-      scaService.deleteUiStateForTask(task.projectKey, task.key, 'crs-concepts').then(function () {
-        currentTaskConcepts = null;
-        deferred.resolve();
-      }, function (error) {
-        deferred.reject('Error deleting crs-concepts ui state');
-      })
-    }
+      //
+      // Clear the CRS Concept list for a task
+      //
+      function clearCrsConceptsForTask(task) {
+        var deferred = $q.defer();
+        scaService.deleteUiStateForTask(task.projectKey, task.key, 'crs-concepts').then(function () {
+          currentTaskConcepts = null;
+          deferred.resolve();
+        }, function (error) {
+          deferred.reject('Error deleting crs-concepts ui state');
+        });
+      }
 
       //
       // Gets the CRS concepts for task, initializing UI states on first attempt
@@ -148,9 +137,8 @@ angular.module('singleConceptAuthoringApp')
       //
       return {
         getCrsConceptsForTask: getCrsConceptsForTask,
-        clearCrsConceptsForTask : clearCrsConceptsForTask
-      }
+        clearCrsConceptsForTask: clearCrsConceptsForTask
+      };
     }
-
-    )
+  )
 ;
