9a7a475e124bffeb5a0a01a7000fcb53cec8e5f2
Parick Granvold <pgranvold@westcoastinformatics.com>
2016-08-05 15:03:24 -0700

    WRP-2937: concept list and service implementations

--- app/shared/crs-concept-list/crsConceptList.js
+++ app/shared/crs-concept-list/crsConceptList.js
@@ -7,36 +7,49 @@ angular.module('singleConceptAuthoringApp')
       transclude: false,
       replace: true,
       scope: {
-        task : '='
+        task: '='
       },
       templateUrl: 'shared/crsConceptList/crsConceptList.html',
 
       link: function (scope) {
 
+        // get a concept properties object for dragging
+        // TODO Handle MS?
+        scope.getConceptPropertiesObj = function (item) {
+          return {id: item.concept.conceptId, name: item.concept.fsn};
+        };
+
         // open a concept from the list
-        scope.openConcept = function(concept) {
+        scope.openConcept = function (concept) {
           $rootScope.$broadcast('editConcept', concept);
         };
 
         // on saveConcept events, mark the concept as saved
-        scope.finishConcept = function(concept) {
+        scope.finishConcept = function (concept) {
           // TODO
-        }
+        };
 
         // completely remove a concept from the list
-        scope.removeConcept = function(concept) {
+        scope.removeConcept = function (concept) {
           // TODO
-        }
+        };
 
 
         //
         // Initialization
         //
         function initialize() {
-
+          crsService.getCrsConceptsForTask(scope.task).then(function (concepts) {
+            scope.concepts = concepts;
+          })
         }
 
-        initialize();
+        // re-initialize on task changes
+        scope.$watch('task', function () {
+          if (scope.task) {
+            initialize();
+          }
+        })
 
       }
     };
