8ca893e8e98462df229f910414dd84ab4eb14fee
Parick Granvold <pgranvold@westcoastinformatics.com>
2016-08-05 09:10:40 -0700

    WRP-2948: Allowable attribute lookup now supports id

--- app/shared/concept-edit/conceptEdit.js
+++ app/shared/concept-edit/conceptEdit.js
@@ -1604,7 +1604,7 @@ angular.module('singleConceptAuthoringApp').directive('conceptEdit', function ($
           }
 
           // check that attribute is acceptable for MRCM rules
-          var attributes = scope.getConceptForFullAttribute(data.name);
+          var attributes = scope.getConceptForFullAttribute(data.id);
           if (attributes && attributes.length > 0) {
             relationship.type.conceptId = data.id;
             relationship.type.fsn = data.name;
@@ -2354,10 +2354,13 @@ angular.module('singleConceptAuthoringApp').directive('conceptEdit', function ($
             // add to id list if: active, Is A relationship, target
             // specified, and not inferred
             if (relationship.active === true && relationship.type.conceptId === '116680003' && relationship.target.conceptId && relationship.characteristicType !== 'INFERRED_RELATIONSHIP') {
+
               idList += relationship.target.conceptId + ',';
+              console.debug('match', idList);
             }
           });
           idList = idList.substring(0, idList.length - 1);
+          console.debug(idList);
 
           snowowlService.getDomainAttributes(scope.branch, idList).then(function (response) {
             scope.allowedAttributes = response.items;
@@ -2409,7 +2412,7 @@ angular.module('singleConceptAuthoringApp').directive('conceptEdit', function ($
             }
           }
           response = response.filter(function (item) {
-            return item.fsn.term.toLowerCase() === searchStr.toLowerCase();
+            return item.fsn.term.toLowerCase() === searchStr.toLowerCase() || item.id === searchStr;
           });
           return response;
         };
