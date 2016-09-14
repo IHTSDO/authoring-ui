0fa0bec5312002ff40a2650a9052dfe8a0f7337b
Parick Granvold <pgranvold@westcoastinformatics.com>
2016-09-14 09:08:42 -0700

    On-demand test notification generation via #/test

--- app/components/test/test.js
+++ app/components/test/test.js
@@ -1,64 +1,43 @@
 'use strict';
 
-angular.module( 'singleConceptAuthoringApp.test', [
+angular.module('singleConceptAuthoringApp.test', [
   //insert dependencies here
-    'ngRoute'
+  'ngRoute'
 ])
 
-.config(function config( $routeProvider ) {
-  $routeProvider
-    .when('/test', {
+  .config(function config($routeProvider) {
+    $routeProvider
+      .when('/test', {
         controller: 'TestCtrl',
         templateUrl: 'components/test/test.html'
       });
-})
+  })
 
-.controller( 'TestCtrl', function TestCtrl( $scope, $rootScope, $interval, QaTestPackage, scaService, metadataService) {
+  .controller('TestCtrl', function TestCtrl($scope, $rootScope, $interval, notificationService) {
 
     $rootScope.pageTitle = 'Test Management';
 
-    $scope.results = {};
+    console.log('Entered TestCtrl');
 
-    $scope.projectKey = null;
-    $scope.taskKey = null;
+    // notification testing variables
+    $scope.notificationType = 'Message';
+    $scope.notificationText = 'Validation completed for task WRPSII-86';
+    $scope.notificationUrl = '#/tasks/task/WRPSII/WRPSII-86/validate';
+    $scope.notificationDuration = 0;
 
-    var task = {
-      summary: 'FE Convention Validation Tests',
-      description: 'Run ' + Date.now().toString()
-    };
-
-    scaService.createTaskForProject('WRPTEST', task)   .then(function(response) {
-      $scope.projectKey = response.projectKey;
-      $scope.taskKey = response.key;
-      $scope.branch = task.branchPath;
-      metadataService.setBranchMetadata(task);
-    });
-
-    $scope.testPackages = [
-      QaTestPackage
-    ];
-
-    // initialize the available tests
-    angular.forEach($scope.testPackages, function (testPackage) {
-      var results = testPackage.getResults();
-      testPackage.results = results;
-    });
+    $scope.sendNotification = function() {
+      switch ($scope.notificationType) {
+        case 'Message':
+          notificationService.sendMessage($scope.notificationText, $scope.notificationDuration, $scope.notificationUrl);
+          break;
+        case 'Warning':
+          notificationService.sendWarning($scope.notificationText, $scope.notificationDuration, $scope.notificationUrl);
+          break;
+        case 'Error':
+          notificationService.sendError($scope.notificationText, $scope.notificationDuration, $scope.notificationUrl);
+          break;
+      }
+    }
 
 
-    $scope.runTest = function(testPackage) {
-      testPackage.runTests($scope.projectKey, $scope.taskKey);
-
-      var resultsPolling = $interval(function() {
-
-        var results = testPackage.getResults();
-        testPackage.results = results;
-      }, 2000);
-    };
-
-    $scope.runSingleTest = function (testPackage, testGroupName, test) {
-      testPackage.runSingleTest(testGroupName, test.name, $scope.projectKey, $scope.taskKey).then(function(response) {
-        test = response;
-      });
-    };
-
-});
+  });
