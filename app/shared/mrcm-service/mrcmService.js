042d57667eb41f8995735ff5c1db28691468df62
Parick Granvold <pgranvold@westcoastinformatics.com>
2016-10-10 10:56:51 -0700

    Added mrcmService (folder/file/index.html)

---
+++ app/shared/mrcm-service/mrcmService.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('singleConceptAuthoringApp')
+  .service('mrcmService', ['$http', '$rootScope', function ($http, $rootScope) {
+
+    // expose functions
+    return {
+
+    }
+  }])
+;
