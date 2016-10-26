8c5e80a37250aba3265a81c69b60cd7c7d3465f5
Parick Granvold <pgranvold@westcoastinformatics.com>
2016-10-26 11:24:41 -0700

    Final review sidebar menu with warnings from reviewService

--- app/shared/review-service/reviewService.js
+++ app/shared/review-service/reviewService.js
@@ -32,10 +32,10 @@ angular.module('singleConceptAuthoringApp')
 
     function cancelReview(task) {
       var deferred = $q.defer();
-      scaService.markTaskInProgress(task.projectKey, task.key).then(function() {
+      scaService.markTaskInProgress(task.projectKey, task.key).then(function () {
         $rootScope.$broadcast('reloadTask');
         deferred.resolve();
-      }, function(error) {
+      }, function (error) {
         deferred.reject();
       });
       return deferred.promise;
@@ -43,101 +43,113 @@ angular.module('singleConceptAuthoringApp')
 
     function unclaimReview(task) {
       var deferred = $q.defer();
-      scaService.unassignReview(task.projectKey, task.key).then(function() {
+      scaService.unassignReview(task.projectKey, task.key).then(function () {
         $rootScope.$broadcast('reloadTask');
         deferred.resolve();
-      }, function(error) {
+      }, function (error) {
         deferred.reject();
       });
       return deferred.promise;
     }
 
-    function getUnsavedContent(task) {
+    function getConceptIdsForTraceability(traceability) {
+      var ids = [];
+      if (!traceability || !traceability.content) {
+        return ids;
+      }
+
+    }
+
+    function checkReviewPrerequisites(task) {
       var deferred = $q.defer();
 
       console.debug('get unsaved content');
 
       // initialize the unsaved concept array
-      var unsavedConcepts = [];
+      var results = {
+        unsavedConcepts: [],
+        hasChangedContent: false
+      };
 
       // first, check if traceability returns changes on this task
       snowowlService.getTraceabilityForBranch(task.branchPath).then(function (traceability) {
 
         console.debug('traceability', traceability);
 
-        if (!traceability || !traceability.numberOfElements || traceability.numberOfElements === 0) {
-          deferred.resolve(unsavedConcepts);
-        } else {
+        // first check -- does traceability exist?
+        if (traceability && traceability.numberOfElements > 0) {
+          results.hasChangedContent = true;
+        }
 
-          console.debug('getting modified concept ids');
+        console.debug('getting modified concept ids');
 
-          // retrieve the modified concepts for this task
-          scaService.getModifiedConceptIdsForTask(task.projectKey, task.key).then(function (conceptIds) {
+        // retrieve the modified concepts for this task
+        scaService.getModifiedConceptIdsForTask(task.projectKey, task.key).then(function (conceptIds) {
 
-            console.debug('modified concept ids', conceptIds);
-            var conceptCt = 0;
+          console.debug('modified concept ids', conceptIds);
+          var conceptCt = 0;
 
-            if (!conceptIds || conceptIds.length == 0) {
-              deferred.resolve([]);
-            }
+          if (!conceptIds || conceptIds.length == 0) {
+            deferred.resolve(results);
+          }
 
-            angular.forEach(conceptIds, function (conceptId) {
-              console.debug('checking ', conceptId);
+          angular.forEach(conceptIds, function (conceptId) {
 
-              // only check for unsaved content on SCTID-marked content
-              if (snowowlService.isSctid(conceptId)) {
-                scaService.getModifiedConceptForTask(task.projectKey, task.key, conceptId).then(function (concept) {
+            // only check for unsaved content on SCTID-marked content
+            if (snowowlService.isSctid(conceptId)) {
+              scaService.getModifiedConceptForTask(task.projectKey, task.key, conceptId).then(function (concept) {
 
-                  // Account for case where new concepts are marked 'current' in UI State
-                  if (concept) {
+                // Account for case where new concepts are marked 'current' in UI State
+                if (concept) {
 
-                    if (!concept.conceptId) {
-                      concept.conceptId = '(New concept)';
-                    }
-                    // find the FSN for display
-                    if (!concept.fsn) {
-                      angular.forEach(concept.descriptions, function (d) {
-                        if (d.type === 'FSN') {
-                          concept.fsn = d.term;
-                        }
-                      })
-                    }
-                    if (!concept.fsn) {
-                      concept.fsn = 'Could not determine FSN';
-                    }
-                    unsavedConcepts.push(concept);
+                  if (!concept.conceptId) {
+                    concept.conceptId = '(New concept)';
                   }
-                  // if no concepts survive processing, proceed with submission
-                  if (++conceptCt === conceptIds.length) {
-                    deferred.resolve(unsavedConcepts);
+                  // find the FSN for display
+                  if (!concept.fsn) {
+                    angular.forEach(concept.descriptions, function (d) {
+                      if (d.type === 'FSN') {
+                        concept.fsn = d.term;
+                      }
+                    })
                   }
-
-                }, function (error) {
-                  deferred.reject('Unexpected error getting modified concept ' + conceptId + ': ' + error);
-                });
-              }
-
-              // otherwise, increment counter and continue
-              else {
+                  if (!concept.fsn) {
+                    concept.fsn = 'Could not determine FSN';
+                  }
+                  results.unsavedConcepts.push(concept);
+                }
                 // if no concepts survive processing, proceed with submission
                 if (++conceptCt === conceptIds.length) {
-                  deferred.resolve(unsavedConcepts);
+                  deferred.resolve(results);
                 }
+
+              }, function (error) {
+                deferred.reject('Unexpected error getting modified concept ' + conceptId + ': ' + error);
+              });
+            }
+
+            // otherwise, increment counter and continue
+            else {
+              // if no concepts survive processing, proceed with submission
+              if (++conceptCt === conceptIds.length) {
+                deferred.resolve(results);
               }
-            });
-
-
-          }, function (error) {
-            console.debug('ERROR getting modified conceptIds', error);
-            deferred.resolve([]);
+            }
           });
-        }
-      }, function (error) {
-        console.error('Could not retrieve traceability when submitting for review', error);
 
-        // empty array for unsaved concepts to bypass null check
-        deferred.resolve([]);
+
+        }, function (error) {
+          console.debug('ERROR getting modified conceptIds', error);
+          deferred.resolve([]);
+        });
+
+
+      }, function (error) {
+        // do nothing
       });
+
+
+
 
       return deferred.promise;
     }
@@ -146,12 +158,12 @@ angular.module('singleConceptAuthoringApp')
 
       // transition functions
       // NOTE: Not complete functionality, developed for sidebar menu initially
-      submitForReview : submitForReview,
-      cancelReview : cancelReview,
-      unclaimReview : unclaimReview,
+      submitForReview: submitForReview,
+      cancelReview: cancelReview,
+      unclaimReview: unclaimReview,
 
       // utility functions
-      getUnsavedContent: getUnsavedContent
+      checkReviewPrerequisites: checkReviewPrerequisites
     }
 
   })
