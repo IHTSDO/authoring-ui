39b37c1e8442119683d4c5d9c9fc92e5b2cf7d8f
Parick Granvold <pgranvold@westcoastinformatics.com>
2016-11-03 11:58:05 -0700

    Test interface for creating/updating templates in tool

--- app/components/test/test.js
+++ app/components/test/test.js
@@ -13,34 +13,48 @@ angular.module('singleConceptAuthoringApp.test', [
       });
   })
 
-  .controller('TestCtrl', function TestCtrl($scope, $rootScope, $interval, notificationService, languageService) {
+  .controller('TestCtrl', function TestCtrl($scope, $rootScope, templateService, notificationService) {
 
       $rootScope.pageTitle = 'Test Management';
+      $scope.name = 'Guided CT of X';
+      $scope.templateMode = 'Get';
+      $scope.templateJson = null;
 
       console.log('Entered TestCtrl');
 
-      // spellcheck testing variables
-      $scope.getTestSpellingWords = languageService.getTestSpellings;
-      $scope.term = 'Hey! You mispelled whiskee!';
-      $scope.term2 = 'You mispelled this one too. Is it the whiskee?';
+    function prettyPrint() {
+      var ugly = $scope.templateJson;
+      var obj = JSON.parse(ugly);
+      var pretty = JSON.stringify(obj, undefined, 4);
+      document.getElementById('prettyJson').value = pretty;
+    }
 
-
-      // notification testing variables
-      $scope.notificationType = 'Message';
-      $scope.notificationText = 'Validation completed for task WRPSII-86';
-      $scope.notificationUrl = '#/tasks/task/WRPSII/WRPSII-86/validate';
-      $scope.notificationDuration = 0;
-
-      $scope.sendNotification = function () {
-        switch ($scope.notificationType) {
-          case 'Message':
-            notificationService.sendMessage($scope.notificationText, $scope.notificationDuration, $scope.notificationUrl);
+      $scope.performAction = function () {
+        console.debug('perform action', $scope.name, $scope.templateMode, $scope.templateJson);
+        switch ($scope.templateMode) {
+          case 'Get':
+            templateService.getTemplateForName($scope.name).then(function (response) {
+              $scope.templateJson = JSON.stringify(response);
+              prettyPrint();
+            }, function (error) {
+              notificationService.sendError('Error: ' + error);
+            });
             break;
-          case 'Warning':
-            notificationService.sendWarning($scope.notificationText, $scope.notificationDuration, $scope.notificationUrl);
+          case 'Create':
+            templateService.createTemplate($scope.templateJson, $scope.name).then(function (response) {
+              $scope.templateJson = JSON.stringify(response);
+              prettyPrint();
+            }, function (error) {
+              notificationService.sendError('Error: ' + error);
+            });
             break;
-          case 'Error':
-            notificationService.sendError($scope.notificationText, $scope.notificationDuration, $scope.notificationUrl);
+          case 'Update':
+            templateService.updateTemplate($scope.templateJson, $scope.name).then(function (response) {
+              $scope.templateJson = JSON.stringify(response);
+              prettyPrint();
+            }, function (error) {
+              notificationService.sendError('Error: ' + error);
+            });
             break;
         }
       }
