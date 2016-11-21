8787935c32997838b8ded379c2e20ca4535bc801
Parick Granvold <pgranvold@westcoastinformatics.com>
2016-11-21 09:37:00 -0800

    Fixed merge-generated problem with loading concepts from edit view

--- app/components/edit/edit.js
+++ app/components/edit/edit.js
@@ -640,8 +640,43 @@ angular.module('singleConceptAuthoringApp.edit', [
           notificationService.sendMessage('Concept loaded', 3000);
         } else {
 
-        if ($scope.concepts.length === $scope.editList.length) {
-          notificationService.sendMessage('All concepts loaded', 10000, null);
+          // get the concept and add it to the stack
+          snowowlService.getFullConcept(conceptId, $scope.targetBranch).then(function (response) {
+            $scope.conceptLoading = false;
+            if (!response) {
+              return;
+            }
+
+            $scope.concepts.push(response);
+
+            if ($scope.editList.indexOf(conceptId) === -1) {
+              $scope.updateEditListUiState();
+            }
+
+            if ($scope.concepts.length === $scope.editList.length) {
+              notificationService.sendMessage('All concepts loaded', 10000, null);
+              // ensure loaded concepts match order of edit list
+              $scope.concepts.sort(function (a, b) {
+                return $scope.editList.indexOf(a.conceptId) > $scope.editList.indexOf(b.conceptId);
+              });
+              $scope.updateEditListUiState();
+            } else {
+              // send loading notification for user display
+              notificationService.sendMessage('Loading concepts...', 10000, null);
+            }
+
+            deferred.resolve();
+
+          }, function (error) {
+            $scope.conceptLoading = false;
+            console.log('Error retrieving concept', error);
+            if (error.status === 404) {
+              notificationService.sendWarning('Concept not found on this branch. If it exists on another branch, promote that branch and try again');
+            } else {
+              notificationService.sendError('Unexpected error retrieving concept');
+            }
+            deferred.reject();
+          });
         }
       });
       return deferred.promise;
