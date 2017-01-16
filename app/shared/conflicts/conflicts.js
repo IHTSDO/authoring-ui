7bd64229d4a773b206e27b07c0e15833e049ca4f
Chris Swires <csw@ihtsdo.org>
2017-01-16 10:33:37 +0000

    Revert "WRP-2376"
    This reverts commit 15d4ed9a81f041bca86f8e97ff8175f9255dee41.

--- app/shared/conflicts/conflicts.js
+++ app/shared/conflicts/conflicts.js
@@ -526,32 +526,30 @@ angular.module('singleConceptAuthoringApp')
             scope.rebaseRunning = true;
 
             if ($routeParams.taskKey) {
-              snowowlService.rebaseBranches(scope.sourceBranch, scope.targetBranch, scope.id).then(function (response) {
-                  $rootScope.$broadcast('pollForRebase', {url: response.locHeader});
+              scaService.rebaseTask($routeParams.projectKey, $routeParams.taskKey).then(function (response) {
+                if (response !== null && response !== 1) {
+                  scope.rebaseRunning = false;
+                  scope.rebaseComplete = true;
+                  scope.warning = false;
+                  scope.fiveOFour = false;
+
+                  // switch to edit view on success
                   exitConflictsView();
-//                if (response !== null && response !== 1) {
-//                  scope.rebaseRunning = false;
-//                  scope.rebaseComplete = true;
-//                  scope.warning = false;
-//                  scope.fiveOFour = false;
-//
-//                  // switch to edit view on success
-//                  exitConflictsView();
-//                }
-//                else if (response === 1) {
-//                  console.log('1');
-//                  scope.rebaseRunning = false;
-//                  scope.rebaseComplete = false;
-//                  scope.warning = false;
-//                  scope.fiveOFour = true;
-//                }
-//                else {
-//                  scope.rebaseRunning = false;
-//                  scope.rebaseComplete = false;
-//                  scope.warning = true;
-//                  $rootScope.canConflict = true;
-//                  scope.fiveOFour = false;
-//                }
+                }
+                else if (response === 1) {
+                  console.log('1');
+                  scope.rebaseRunning = false;
+                  scope.rebaseComplete = false;
+                  scope.warning = false;
+                  scope.fiveOFour = true;
+                }
+                else {
+                  scope.rebaseRunning = false;
+                  scope.rebaseComplete = false;
+                  scope.warning = true;
+                  $rootScope.canConflict = true;
+                  scope.fiveOFour = false;
+                }
 
               }, function (error) {
                 scope.rebaseRunning = false;
@@ -562,37 +560,35 @@ angular.module('singleConceptAuthoringApp')
               });
             } else {
 
-              snowowlService.rebaseBranches(scope.sourceBranch, scope.targetBranch, scope.id).then(function (response) {
-                $rootScope.$broadcast('pollForRebase', {url: response.locHeader});
+              scaService.rebaseProject($routeParams.projectKey).then(function (response) {
+                if (response !== null && response !== 1) {
+                  scope.rebaseRunning = false;
+                  scope.rebaseComplete = true;
+                  scope.warning = false;
+                  scope.fiveOFour = false;
+
+                  // switch to edit view on success
                   exitConflictsView();
-//                if (response !== null && response !== 1) {
-//                  scope.rebaseRunning = false;
-//                  scope.rebaseComplete = true;
-//                  scope.warning = false;
-//                  scope.fiveOFour = false;
-//
-//                  // switch to edit view on success
-//                  exitConflictsView();
-//
-//                }
-//                else if (response === 1) {
-//                  scope.rebaseRunning = false;
-//                  scope.rebaseComplete = false;
-//                  scope.warning = false;
-//                  scope.fiveOFour = true;
-//
-//                  // NOTE: Do not switch to edit view on error
-//
-//                }
-//                else {
-//                  scope.rebaseRunning = false;
-//                  scope.rebaseComplete = false;
-//                  scope.warning = true;
-//                  scope.fiveOFour = false;
-//
-//                  // NOTE: Do not switch to edit view on warning
-//                  // TODO Need to revisit this
-//                }
+
+                }
+                else if (response === 1) {
+                  scope.rebaseRunning = false;
+                  scope.rebaseComplete = false;
+                  scope.warning = false;
+                  scope.fiveOFour = true;
+
+                  // NOTE: Do not switch to edit view on error
+
+                }
+                else {
+                  scope.rebaseRunning = false;
+                  scope.rebaseComplete = false;
+                  scope.warning = true;
+                  scope.fiveOFour = false;
+
+                  // NOTE: Do not switch to edit view on warning
+                  // TODO Need to revisit this
+                }
               }, function (error) {
                 scope.rebaseRunning = false;
                 scope.rebaseComplete = false;
