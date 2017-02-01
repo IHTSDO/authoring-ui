92ad6c0d649771fdd01cf3ca4772541abee041bd
Chris Swires <csw@ihtsdo.org>
2017-02-01 12:21:53 +0000

    APDS-18 - tweak for MS inclusion of DUPLICATE as a description inactivation reason

--- app/shared/metadata-service/metadataService.js
+++ app/shared/metadata-service/metadataService.js
@@ -249,7 +249,14 @@ angular.module('singleConceptAuthoringApp')
 //            })
 //        }
         if(getCurrentModuleId() !== '900000000000207008'){
-            descriptionInactivationReasons.push({id: 'DUPLICATE', text: 'Duplicate component'});
+            for(var i = 0; i < descriptionInactivationReasons.length; i++) {
+                if (vendors[i].id == 'DUPLICATE') {
+                    found = true;
+                }
+                if(i === descriptionInactivationReasons.length && !found){
+                    descriptionInactivationReasons.push({id: 'DUPLICATE', text: 'Duplicate component'});
+                }
+            }
         }
         $rootScope.$broadcast('setExtensionMetadata');
 
