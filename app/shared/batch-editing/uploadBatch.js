9c9609d25e7eac40d43dd95455f7a65ed473c7e5
Chris Swires <csw@ihtsdo.org>
2017-02-21 15:00:04 +0000

    TBBA generic - removal of 404, race condition in uploadBatch

--- app/shared/batch-editing/uploadBatch.js
+++ app/shared/batch-editing/uploadBatch.js
@@ -11,7 +11,7 @@ angular.module('singleConceptAuthoringApp.uploadBatch', [])
         
          var conceptPromises = [];
         
-         $scope.isBatchPopulated = batchEditingService.getBatchConcepts().length === 0;
+         $scope.isBatchPopulated = batchEditingService.getBatchConcepts() && batchEditingService.getBatchConcepts().length === 0;
         
          $scope.dlcDialog = (function (data, fileName) {
             var a = document.createElement('a');
