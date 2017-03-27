aea75e90318624f6d6e593cc7a1174c1e6ef1416
Chris Swires <csw@ihtsdo.org>
2017-03-27 17:39:18 +0100

    TBBA-162, TBBA-163

--- app/components/edit/edit.js
+++ app/components/edit/edit.js
@@ -1368,7 +1368,7 @@ angular.module('singleConceptAuthoringApp.edit', [
     // declare table parameters
     $scope.templateTableParams = new ngTableParams({
         page: 1,
-        count: 100,
+        count: 200,
         sorting: {name: 'asc'}
       },
       {
