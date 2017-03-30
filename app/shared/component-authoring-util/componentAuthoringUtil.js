1a650243930ea2887c16cd016fdaa2a012e3c4d6
Chris Swires <csw@ihtsdo.org>
2017-03-30 15:18:41 +0100

    APDS-121

--- app/shared/component-authoring-util/componentAuthoringUtil.js
+++ app/shared/component-authoring-util/componentAuthoringUtil.js
@@ -362,7 +362,7 @@ angular.module('singleConceptAuthoringApp')
           var ptText = description.term.substr(0, description.term.lastIndexOf('(')).trim();
           var pt = null;
           angular.forEach(concept.descriptions, function (d) {
-            if (d.type === 'SYNONYM' && d.acceptabilityMap && d.acceptabilityMap['900000000000509007'] === 'PREFERRED') {
+            if (d.type === 'SYNONYM' && d.acceptabilityMap && d.acceptabilityMap['900000000000509007'] === 'PREFERRED' && d.released === false) {
               if (d.term && d.term !== '' && d.term !== null) {
                 pt = d;
               }
