6277819b8c5089d7aaa50d0574f8836052797706
Chris Swires <csw@ihtsdo.org>
2017-08-07 14:33:25 +0100

    TBBA-195

--- app/shared/search/search.js
+++ app/shared/search/search.js
@@ -1,6 +1,20 @@
 'use strict';
 angular.module('singleConceptAuthoringApp.searchPanel', [])
 
+.directive('focusOn',function($timeout) {
+    return {
+        restrict : 'A',
+        link : function($scope,$element,$attr) {
+            $scope.$watch($attr.focusOn,function(_focusVal) {
+                $timeout(function() {
+                    _focusVal ? $element[0].focus() :
+                        $element[0].blur();
+                });
+            });
+        }
+    }
+})
+
   .controller('searchPanelCtrl', ['$scope', '$rootScope', '$modal', '$location', '$routeParams', '$q', '$http', 'metadataService', 'notificationService', 'scaService', 'snowowlService', 'templateService', 'batchEditingService', 'modalService',
     function searchPanelCtrl($scope, $rootScope, $modal, $location, $routeParams, $q, $http, metadataService, notificationService, scaService, snowowlService, templateService, batchEditingService, modalService) {
 
