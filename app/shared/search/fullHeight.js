1d0f404fa5956baf683d7fa40a7bdc304b3787e5
Quyen Ly <Quyen.Ly@Huntermacdonald.com>
2017-09-05 18:54:39 +0700

    APDS-330  extend the space that is used for displaying the search results using Angular Directive instead of responsive css

--- app/shared/search/fullHeight.js
+++ app/shared/search/fullHeight.js
@@ -7,45 +7,30 @@
   .directive('fullHeight', ['$parse', function($parse) {
     return {
       scope: {
-          additionalFields: '@?' // Specify more additional fields if it's take into account When calculating height for element, 
+          additionalPaddingBottom: '@?' // Specify more additional padding bottom
       },
       link: function(scope, elem, attrs) {
         var fn = $parse(attrs.fullHeight);
         var eleHeight = 0;
         var posEnd = 0;
-        var additionalPadding = 123;        
-
+        var additionalPaddingBottom = 0;
+        if(scope.additionalPaddingBottom) {
+          additionalPaddingBottom = scope.additionalPaddingBottom;
+        }
+       
         scope.$watch(function() {
           return elem.height();
-        }, function(newHeight) {        
-          adjustElementHeight(newHeight);
+        }, function(newHeight) {
+          if(newHeight > 0) {
+            eleHeight = newHeight;
+            var posStart = angular.element(elem).prop('offsetTop');
+            var endElem = angular.element(document.querySelector(".sca-footer"));
+            posEnd = angular.element(endElem).prop('offsetTop');
+            var newEleHeight = posEnd - posStart - additionalPaddingBottom;            
+            elem.attr( 'style', 'max-height: ' + newEleHeight + 'px;');
+            
+          }                   
         }, true);
-
-        scope.$on('adjustElementHeight', function (event, data) {
-          adjustElementHeight();
-        });
-
-        function adjustElementHeight(newHeight) {
-          var posStart = angular.element(elem).prop('offsetTop');
-          
-          var endElem = angular.element(document.querySelector(".sca-footer"));
-          posEnd = angular.element(endElem).prop('offsetTop');          
-
-          var additionalHeight = 0;
-          if(scope.additionalFields){
-            var additionalArr = scope.additionalFields.split(',');
-            additionalArr.forEach(function(item){
-              var additionalElm = angular.element(document.querySelector("." + item));  
-              additionalHeight += additionalElm.height();
-            });           
-          }         
-
-          if(typeof newHeight  === 'undefined'
-            || (newHeight > 0 && newHeight > posEnd)) {
-            eleHeight = posEnd - posStart - additionalHeight - additionalPadding;
-            elem.attr( 'style', 'height: ' + eleHeight + 'px;');
-          }
-        }        
       }
     }
   }])
