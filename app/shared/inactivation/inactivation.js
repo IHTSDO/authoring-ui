7f42afe2612474e708f1ee3a3fad4239429679fd
Quyen Ly <Quyen.Ly@Huntermacdonald.com>
2017-09-29 17:16:07 +0700

    APDS-375 Handle incoming historical association for Nonconformance to editorial policy

--- app/shared/inactivation/inactivation.js
+++ app/shared/inactivation/inactivation.js
@@ -472,7 +472,37 @@ angular.module('singleConceptAuthoringApp')
                 var data = scope.affectedConceptAssocs ? scope.affectedConceptAssocs : [];
                 params.total(data.length);
                 data = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
-                $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                
+                // Look up NONCONFORMANCE_TO_EDITORIAL_POLICY
+                var nonConformanceConcepts = data.filter(function (item) {
+                  return item.inactivationIndicator === 'NONCONFORMANCE_TO_EDITORIAL_POLICY';
+                });
+
+                if (nonConformanceConcepts.length > 0) {
+                  var promises = [];
+                  nonConformanceConcepts.forEach(function (item) {
+                    promises.push(snowowlService.getFullConcept(item.conceptId, scope.branch));
+                  });
+                   
+                  // on resolution of all promises
+                  $q.all(promises).then(function (historicalConcepts) {
+                    nonConformanceConcepts.forEach(function(item){
+                      for (var i = 0; i < historicalConcepts.length; i++) {
+                        var historicalConcept = historicalConcepts[i];
+                        if(item.conceptId === historicalConcept.conceptId) {
+                          item.inactivationIndicator = historicalConcept.inactivationIndicator;
+                          item.newTargetFsn = scope.inactivationConcept.fsn;
+                          item.newTargetId = scope.inactivationConcept.conceptId;
+                        }
+                      }
+                    });
+                    $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                  }, function (error) {
+                    $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));                   
+                  });
+                } else {
+                  $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                }                
               }
             }
           );
@@ -1036,6 +1066,17 @@ angular.module('singleConceptAuthoringApp')
             }
             console.log(rel);
           };
+
+          scope.hasNoConceptTarget = function () {           
+            for (var i = 0; i < scope.affectedConceptAssocs.length; i++) {
+              var concept = scope.affectedConceptAssocs[i];
+              if (!concept.newTargetId || !concept.newTargetFsn || !concept.refsetName) {
+                return true;
+              }
+            }
+            return false;
+          }
+
 
 
           //
