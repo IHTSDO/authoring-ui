e137d811836727cb53cb4f9e8dbb9e05a57173d7
Peter Duff <pdu@ihtsdo.org>
2018-01-23 17:04:37 +0000

    APDS-265
    Now returning the correct number.

--- app/shared/search/search.js
+++ app/shared/search/search.js
@@ -372,28 +372,42 @@ angular.module('singleConceptAuthoringApp.searchPanel', [])
           // set as scope variable for expected future toggle
           $scope.synonymFlag = metadataService.isExtensionSet();
 
-          snowowlService.findConceptsForQuery($routeParams.projectKey, $routeParams.taskKey, $scope.searchStr, $scope.results.length, $scope.resultsSize, acceptLanguageValue, $scope.synonymFlag).then(function (concepts) {
+          $scope.searchTotal = null;
 
-            if (!concepts) {
-              notificationService.sendError('Unexpected error searching for concepts', 10000);
-            }
-
-            // set load more parameters
-            $scope.loadPerformed = true;
-            $scope.loadMoreEnabled = concepts.length === $scope.resultsSize;
-
-            $scope.storedResults = appendResults ? $scope.storedResults.concat(concepts) : concepts;
-
-            $scope.processResults();
-
+          snowowlService.searchConcepts($scope.branch, $scope.searchStr, $scope.escgExpr, $scope.results.length, $scope.resultsSize, $scope.synonymFlag).then(function (results) {
+            return results;
           }, function (error) {
-            $scope.searchStatus = 'Error performing search: ' + error.message;
+            $scope.searchStatus = 'Error performing search: ' + error;
             if (error.statusText) {
               $scope.searchStatus += ': ' + error.statusText;
             }
             if (error.data && error.data.message) {
               $scope.searchStatus += ': ' + error.data.message;
             }
+          }).then(function(results) {
+            snowowlService.findConceptsForQuery($routeParams.projectKey, $routeParams.taskKey, $scope.searchStr, $scope.results.length, $scope.resultsSize, acceptLanguageValue, $scope.synonymFlag).then(function (concepts) {
+
+              if (!concepts) {
+                notificationService.sendError('Unexpected error searching for concepts', 10000);
+              }
+
+              // set load more parameters
+              $scope.loadPerformed = true;
+              $scope.loadMoreEnabled = concepts.length === $scope.resultsSize;
+              $scope.storedResults = appendResults ? $scope.storedResults.concat(concepts) : concepts;
+              $scope.searchTotal = results.total;
+
+              $scope.processResults();
+
+            }, function (error) {
+              $scope.searchStatus = 'Error performing search: ' + error.message;
+              if (error.statusText) {
+                $scope.searchStatus += ': ' + error.statusText;
+              }
+              if (error.data && error.data.message) {
+                $scope.searchStatus += ': ' + error.data.message;
+              }
+            });
           });
         } else {
 
