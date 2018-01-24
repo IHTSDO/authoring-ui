0b0d203cb42853566e16a12726689bfb4a575593
Peter Duff <pdu@ihtsdo.org>
2018-01-24 11:47:37 +0000

    APDS-265 Cleanup
    Cleaned up redundant code.

--- app/shared/search/search.js
+++ app/shared/search/search.js
@@ -362,139 +362,71 @@ angular.module('singleConceptAuthoringApp.searchPanel', [])
           acceptLanguageValue = metadataService.getAcceptLanguageValueForModuleId(metadataService.getInternationalModuleId());
         }
 
-        if(!$scope.isEscgMode) {
-          console.debug('normal text mode');
+        // set the return synonym flag to true for extensions
+        // TODO Later this will be toggle-able between extension synonym and fsn
+        // For now, just assume always want the extension-language synonym if available
+        // set as scope variable for expected future toggle
+        $scope.synonymFlag = metadataService.isExtensionSet();
+        $scope.searchTotal = null;
 
-          // set the return synonym flag to true for extensions
-          // TODO Later this will be toggle-able between extension synonym and fsn
-          // For now, just assume always want the extension-language synonym if available
-          // set as scope variable for expected future toggle
-          $scope.synonymFlag = metadataService.isExtensionSet();
-          $scope.searchTotal = null;
+        let escgExpr = $scope.templateOptions.selectedTemplate ? $scope.templateOptions.selectedSlot.allowableRangeECL : $scope.escgExpr;
 
-          let escgExpr = $scope.templateOptions.selectedTemplate ? $scope.templateOptions.selectedSlot.allowableRangeECL : $scope.escgExpr;
+        snowowlService.searchConcepts($scope.branch, $scope.searchStr, escgExpr, $scope.results.length, $scope.resultsSize, $scope.synonymFlag).then(function(results) {
+          if(!results) {
+            notificationService.sendError('Unexpected error searching for concepts', 10000);
+          }
 
-          snowowlService.searchConcepts($scope.branch, $scope.searchStr, escgExpr, $scope.results.length, $scope.resultsSize, $scope.synonymFlag).then(function(results) {
-            if(!results) {
-              notificationService.sendError('Unexpected error searching for concepts', 10000);
-            }
+          let concepts = results.items;
+          $scope.searchTotal = results.total;
+          $scope.loadPerformed = true;
+          $scope.loadMoreEnabled = concepts.length === $scope.resultsSize;
 
-            let concepts = results.items;
-            $scope.searchTotal = results.total;
-            $scope.loadPerformed = true;
-            $scope.loadMoreEnabled = concepts.length === $scope.resultsSize;
-
-
-            // convert to snowowl description search conceptObj
-            let conceptObjs = [];
-            if($scope.synonymFlag){
-              angular.forEach(concepts, function (c) {
-                conceptObjs.push({
+          // convert to snowowl description search conceptObj
+          let conceptObjs = [];
+          if($scope.synonymFlag){
+            angular.forEach(concepts, function (c) {
+              conceptObjs.push({
+                active: c.active,
+                concept: {
                   active: c.active,
-                  concept: {
-                    active: c.active,
-                    conceptId: c.id,
-                    definitionStatus: c.definitionStatus,
-                    fsn: c.pt.term,
-                    moduleId: c.moduleId,
-                    preferredSynonym : c.pt.term
-                  },
-                  term: c.pt.term
-                });
+                  conceptId: c.id,
+                  definitionStatus: c.definitionStatus,
+                  fsn: c.pt.term,
+                  moduleId: c.moduleId,
+                  preferredSynonym : c.pt.term
+                },
+                term: c.pt.term
               });
-            }
-            else{
-              angular.forEach(concepts, function (c) {
-                conceptObjs.push({
+            });
+          }
+          else{
+            angular.forEach(concepts, function (c) {
+              conceptObjs.push({
+                active: c.active,
+                concept: {
                   active: c.active,
-                  concept: {
-                    active: c.active,
-                    conceptId: c.id,
-                    definitionStatus: c.definitionStatus,
-                    fsn: c.fsn.term,
-                    moduleId: c.moduleId
-                  },
-                  term: c.fsn.term
-                });
+                  conceptId: c.id,
+                  definitionStatus: c.definitionStatus,
+                  fsn: c.fsn.term,
+                  moduleId: c.moduleId
+                },
+                term: c.fsn.term
               });
-            }
+            });
+          }
 
-            $scope.storedResults = appendResults ? $scope.storedResults.concat(conceptObjs) : conceptObjs;
+          $scope.storedResults = appendResults ? $scope.storedResults.concat(conceptObjs) : conceptObjs;
+          $scope.processResults();
 
-            $scope.processResults();
-
-          }, function(error) {
-            $scope.searchStatus = 'Error performing search: ' + error;
-            if (error.statusText) {
-              $scope.searchStatus += ': ' + error.statusText;
-            }
-            if (error.data && error.data.message) {
-              $scope.searchStatus += ': ' + error.data.message;
-            }
-          });
-        } else {
-
-          $scope.synonymFlag = metadataService.isExtensionSet();
-          console.debug('escg search', $scope.searchStr, $scope.escgExpr, $scope.templateOptions);
-
-          let escgExpr = $scope.templateOptions.selectedTemplate ? $scope.templateOptions.selectedSlot.allowableRangeECL : $scope.escgExpr;
-
-          snowowlService.searchConcepts($scope.branch, $scope.searchStr, escgExpr, $scope.results.length, $scope.resultsSize, $scope.synonymFlag).then(function (results) {
-            // set load more parameters
-            var concepts = results.items;
-            $scope.searchTotal = results.total;
-            $scope.loadPerformed = true;
-            $scope.loadMoreEnabled = concepts.length === $scope.resultsSize;
-
-
-            // convert to snowowl description search conceptObj
-            var conceptObjs = [];
-            if($scope.synonymFlag){
-                angular.forEach(concepts, function (c) {
-                  conceptObjs.push({
-                    active: c.active,
-                    concept: {
-                      active: c.active,
-                      conceptId: c.id,
-                      definitionStatus: c.definitionStatus,
-                      fsn: c.pt.term,
-                      moduleId: c.moduleId,
-                      preferredSynonym : c.pt.term
-                    },
-                    term: c.pt.term
-                  });
-                });
-            }
-            else{
-                angular.forEach(concepts, function (c) {
-                  conceptObjs.push({
-                    active: c.active,
-                    concept: {
-                      active: c.active,
-                      conceptId: c.id,
-                      definitionStatus: c.definitionStatus,
-                      fsn: c.fsn.term,
-                      moduleId: c.moduleId
-                    },
-                    term: c.fsn.term
-                  });
-                });
-            }
-
-
-            $scope.storedResults = appendResults ? $scope.storedResults.concat(conceptObjs) : conceptObjs;
-
-            $scope.processResults();
-          }, function (error) {
-            $scope.searchStatus = 'Error performing search: ' + error;
-            if (error.statusText) {
-              $scope.searchStatus += ': ' + error.statusText;
-            }
-            if (error.data && error.data.message) {
-              $scope.searchStatus += ': ' + error.data.message;
-            }
-          });
-        }
+        }, function(error) {
+          $scope.searchStatus = 'Error performing search: ' + error;
+          if (error.statusText) {
+            $scope.searchStatus += ': ' + error.statusText;
+          }
+          if (error.data && error.data.message) {
+            $scope.searchStatus += ': ' + error.data.message;
+          }
+        });
       };
 
       /**
