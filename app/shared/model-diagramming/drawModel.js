92dcf4aa05b597a085869803f4414e513b82f58c
QuyenLy <quyen.ly@huntermacdonald.com>
2018-05-28 10:39:50 +0700

    MAINT-118 align circles

--- app/shared/model-diagramming/drawModel.js
+++ app/shared/model-diagramming/drawModel.js
@@ -77,6 +77,10 @@ angular.module('singleConceptAuthoringApp')
                 if (field.type.conceptId === '116680003') {
                   svgIsaModel.push(field);
                 } else {
+                  if(field.groupId > scope.numberOfGroups)
+                  {
+                    scope.numberOfGroups = field.groupId;
+                  }
                   svgAttrModel.push(field);
                 }
               }
@@ -348,7 +352,7 @@ angular.module('singleConceptAuthoringApp')
               else{
                   if (metadataService.isSelfGroupAttribute(relationship.type.conceptId)) {
                     y = y + 20;
-                    x = x + 20;
+                    x = x + (scope.numberOfGroups > 0 ? 0 : 20);
                     var circleSelfgroupAttr = drawAttributeGroupNode(svg, x, y);
                     connectElements(svg, circle2, circleSelfgroupAttr, 'center', 'left');
                     y = y - 20;                
@@ -358,7 +362,7 @@ angular.module('singleConceptAuthoringApp')
                     x = x + rectAttr.getBBox().width + 50;
                     var rectTarget = drawSctBox(svg, x, y, relationship.target.fsn, relationship.target.conceptId, sctClass);
                     connectElements(svg, rectAttr, rectTarget, 'right', 'left'); 
-                    x = x - (circleSelfgroupAttr.getBBox().width + rectAttr.getBBox().width + 110);                
+                    x = x - (circleSelfgroupAttr.getBBox().width + rectAttr.getBBox().width + 90 + (scope.numberOfGroups > 0 ? 0 : 20) );                
                     y = y + rectTarget.getBBox().height + 25;
                     maxX = ((maxX < x + 20 + circleSelfgroupAttr.getBBox().width + 50 + rectAttr.getBBox().width + 50) ? x + rectAttr.getBBox().width + 50 + rectTarget.getBBox().width + 50 : maxX);                    
                   } else {
