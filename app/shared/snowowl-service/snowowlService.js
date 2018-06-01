3e7ac6d974fe23df317040022b223458343afe83
Peter Duff <pdu@ihtsdo.org>
2018-06-01 10:53:03 +0100

    Revert "MAINT-230 - modification of search result parsing"
    This reverts commit cc73921d2f5f4dd464d26e99a8b402a62d05ac08.

--- app/shared/snowowl-service/snowowlService.js
+++ app/shared/snowowl-service/snowowlService.js
@@ -894,7 +894,7 @@ angular.module('singleConceptAuthoringApp')
           active: data.active,
           concept: {
             active: data.active,
-            conceptId: data.id ? data.id : data.fsn.conceptId,
+            conceptId: data.fsn ? data.fsn.conceptId : data.pt.conceptId,
             definitionStatus: data.definitionStatus,
             fsn: data.fsn ? data.fsn.term : data.fsn,
             preferredSynonym: data.pt ? data.pt.term : data.pt,
