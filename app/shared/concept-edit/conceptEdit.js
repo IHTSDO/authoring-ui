11f34e6a92d167fa81208145ea5f4c0603ac3957
Peter Duff <pdu@ihtsdo.org>
2018-06-05 11:21:08 +0100

    MAINT-233 Solved

--- app/shared/concept-edit/conceptEdit.js
+++ app/shared/concept-edit/conceptEdit.js
@@ -2609,7 +2609,7 @@ angular.module('singleConceptAuthoringApp').directive('conceptEdit', function ($
 
           if(data.concept) {
             data.id = data.concept.conceptId;
-            data.name = data.concept.fsn;
+            data.name = data.concept.fsn ? data.concept.fsn : data.concept.preferredSynonym;
           }
 
           // cancel if static
@@ -2761,7 +2761,7 @@ angular.module('singleConceptAuthoringApp').directive('conceptEdit', function ($
 
           if(data.concept) {
             data.id = data.concept.conceptId;
-            data.name = data.concept.fsn;
+            data.name = data.concept.fsn ? data.concept.fsn : data.concept.preferredSynonym;
           }
 
           // cancel if static
