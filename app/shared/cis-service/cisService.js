67326c6e17c93453a8780b9b55eb8ba0c68b1f4f
Chris Swires <csw@ihtsdo.org>
2018-06-05 12:36:39 +0100

    MAINT-159 - draw cis endpoint from ui-config

--- app/shared/cis-service/cisService.js
+++ app/shared/cis-service/cisService.js
@@ -14,13 +14,7 @@ angular.module('singleConceptAuthoringApp')
       // TODO Move this into endpoint config
       //
       function getCisUrl() {       
-        if ($rootScope.development) {
-          return 'https://dev-cis.ihtsdotools.org/api/sct';
-        } else if ($rootScope.uat) {
-          return 'https://uat-cis.ihtsdotools.org/api/sct';
-        } else {
-          return 'https://cis.ihtsdotools.org/api/sct';
-        }
+        return $rootScope.endpoints.cisEndpoint;
       }      
 
       function getAllNamespaces() {
