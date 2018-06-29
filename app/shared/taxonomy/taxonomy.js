b2f53e5ff8e3b1c2f2e0360126811a871e758771
Quyen Ly <quyen.ly@huntermacdonald.com>
2018-06-29 14:37:02 +0700

    MAINT-21 Add second taxonomy

--- app/shared/taxonomy/taxonomy.js
+++ app/shared/taxonomy/taxonomy.js
@@ -1,13 +1,14 @@
 'use strict';
 angular.module('singleConceptAuthoringApp.taxonomyPanel', [])
 
-  .controller('taxonomyPanelCtrl', ['$scope', '$rootScope', '$location', '$routeParams', 'metadataService',
-    function taxonomyPanelCtrl($scope, $rootScope, $location, $routeParams, metadataService) {
+  .controller('taxonomyPanelCtrl', ['$q', '$scope', '$rootScope', '$location', '$routeParams', 'metadataService', 'snowowlService',
+    function taxonomyPanelCtrl($q, $scope, $rootScope, $location, $routeParams, metadataService, snowowlService) {
 
       $scope.branch = metadataService.getBranch();
 
       // initialize with root concept (triggers rendering of full SNOMEDCT hierarchy)
       $scope.rootConcept = null;
+      $scope.secondRootConcept = null;
 
       /**
        * Drag and drop object
@@ -20,10 +21,46 @@ angular.module('singleConceptAuthoringApp.taxonomyPanel', [])
 
       // watch for viewTaxonomy events
       $scope.$on('viewTaxonomy', function(event, data) {
-         $scope.rootConcept = data.concept;
+        $scope.rootConcept = data.concept;
+        $scope.secondRootConcept = null;         
       });
 
       $scope.clearConcept = function() {
         $scope.rootConcept = null;
       };
+
+      $scope.dropConcept = function (concept) {
+        if ($scope.thisView === 'feedback') {
+          checkInferredRelationships($scope.rootConcept.conceptId).then(function(flag) {
+            if (flag) {
+              $scope.secondRootConcept = {conceptId: concept.id,fsn: concept.name};
+            } else {
+              $scope.secondRootConcept = null;
+            }
+          })
+        } else {
+          $scope.secondRootConcept = {conceptId: concept.id,fsn: concept.name};
+        }       
+      }
+
+      $scope.closeTaxonomy = function () {
+        $scope.secondRootConcept = null;
+      }
+
+      function checkInferredRelationships (conceptId) {
+          var deferred = $q.defer();
+          var searchExtensionFlag = metadataService.isExtensionSet();
+          var acceptLanguageValue = metadataService.getAcceptLanguageValueForModuleId(
+          searchExtensionFlag ? metadataService.getCurrentModuleId() : metadataService.getInternationalModuleId());          
+
+          snowowlService.getConceptParents(conceptId, $scope.branch, acceptLanguageValue, searchExtensionFlag, false).then(function (parents) {             
+              deferred.resolve(parents ? true : false);
+            },
+            function () {
+              console.error('Could not retrieve parents for node', node);
+              deferred.resolve(false);
+            }
+          );
+          return deferred.promise;
+        };
     }]);
