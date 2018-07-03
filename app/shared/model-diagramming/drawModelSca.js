d22acd770e008a3d13a2804cb006d8ad0375a74a
Quyen Ly <quyen.ly@huntermacdonald.com>
2018-07-03 11:02:42 +0700

    MAINT-20 Increase size of the displayed font

--- app/shared/model-diagramming/drawModelSca.js
+++ app/shared/model-diagramming/drawModelSca.js
@@ -67,6 +67,45 @@ angular.module('singleConceptAuthoringApp')
         scope.openImage = function() {
           rootScope.$broadcast('openDrawModelConceptImage', {conceptId : scope.concept.conceptId});
         }
+
+        // Zoom in/out function
+        scope.zoomPercentage = 100;
+        var originalSCAModelWidth = null;
+        var zoomArray = [25,33,50,67,75,80,90,100,110,125,150,175,200,250,300,400,500];
+        scope.zoom = function(indicator) {
+          var scaModel = $('#image-' + scope.concept.conceptId);
+          var parentTag = $(scaModel).parent().get(0);
+          var height = $(parentTag).height();
+          var width = $(parentTag).width();
+          if( $(parentTag).css('max-height') === 'none' ) {
+            $(parentTag).css('max-height', height + 'px');
+          }
+          if( $(parentTag).css('max-width') === 'none' ) {
+            $(parentTag).css('max-width', width + 'px');
+          }
+          if (!originalSCAModelWidth) {
+            originalSCAModelWidth = $(scaModel).width();
+          }
+
+          var index = zoomArray.indexOf(scope.zoomPercentage);
+          if (indicator === 'in') {
+            if (zoomArray[index + 1]) {
+              scope.zoomPercentage = zoomArray[index + 1];
+            }
+          } else if (indicator === 'out'){
+            if (zoomArray[index - 1]) {
+              scope.zoomPercentage = zoomArray[index - 1];
+            }
+          } else {
+            // do nothing
+          }
+          $(scaModel).css('max-height','unset');
+          $(scaModel).css('max-width', 'unset');
+          if (index !== -1) {
+            $(scaModel).css('width', (originalSCAModelWidth * scope.zoomPercentage / 100) +  'px');
+          }
+        };
+        
       }
     }
   }]);
