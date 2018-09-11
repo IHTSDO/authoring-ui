84e132810375d00bee81d65d49860f52951b02f7
Quyen Ly <quyen.ly@netcompany.com>
2018-09-11 11:24:06 +0700

    MAINT-270 Fix issue which Drag and drop icon is not consistent

--- app/app.js
+++ app/app.js
@@ -335,6 +335,64 @@ angular
 
   }]);
 
+// Override method dragImage of ang-drag-drop (bower_components/angular-native-dragdrop/draganddrop.js)
+// in order to prevent text render when draging
+angular.module('ang-drag-drop')
+  .constant('$dragImageConfig', {
+        height: 20,
+        width: 200,
+        padding: 10,
+        font: 'bold 11px Arial',
+        fontColor: '#eee8d5',
+        backgroundColor: '#93a1a1',
+        xOffset: 0,
+        yOffset: 0
+    })
+  .service('$dragImage', ['$dragImageConfig', function(defaultConfig) {
+        var ELLIPSIS = '…';
+
+        function fitString(canvas, text, config) {
+            var width = canvas.measureText(text).width;
+            if (width < config.width) {
+                return text;
+            }
+            while (width + config.padding > config.width) {
+                text = text.substring(0, text.length - 1);
+                width = canvas.measureText(text + ELLIPSIS).width;
+            }
+            return text + ELLIPSIS;
+        }
+
+        this.generate = function(text, options) {
+            var config = angular.extend({}, defaultConfig, options || {});
+            var el = document.createElement('canvas');
+
+            el.height = 0;
+            el.width = 0;
+
+            var canvas = el.getContext('2d');
+
+            canvas.fillStyle = config.backgroundColor;
+            canvas.fillRect(0, 0, config.width, config.height);
+            canvas.font = config.font;
+            canvas.fillStyle = config.fontColor;
+
+            var title = fitString(canvas, text, config);
+            canvas.fillText(title, 4, config.padding + 4);
+
+            var image = new Image();
+            image.src = el.toDataURL();
+
+            return {
+                image: image,
+                xOffset: config.xOffset,
+                yOffset: config.yOffset
+            };
+        };
+    }
+    ]
+  );
+
 // Extend the $exceptionHandler service to also display a toast.
 function extendExceptionHandler($delegate, $window) {
   return function (exception, cause) {
