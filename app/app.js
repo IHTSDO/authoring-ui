9f78f6f1bfeb0bd0fbae96a320a3919a6a3dfb9d
Quyen Ly <quyen.ly@netcompany.com>
2019-01-24 10:39:05 +0700

    MAINT-627 Block editing of axioms in SNOMED core module in MS

--- app/app.js
+++ app/app.js
@@ -185,14 +185,6 @@ angular
         snowowlService.setEndpoint(endpoints.terminologyServerEndpoint);
         var accountUrl = endpoints.imsEndpoint + '/auth';
         var imsUrl = endpoints.imsEndpoint;
-        if(!endpoints.axiomDisabled){
-            console.log("false")
-            $rootScope.axiomSupport = true;
-        }
-        else{
-            console.log("true")
-            $rootScope.axiomSupport = false;
-        }
         $rootScope.collectorUrl = $sce.trustAsResourceUrl(endpoints.collectorEndpoint);
         $("<script>").attr({src: $rootScope.collectorUrl}).appendTo("body");
         var imsUrlParams = '?serviceReferer=' + window.location.href;
@@ -291,7 +283,7 @@ angular
       function (error) {
         notificationService.sendError('Fatal error: ' + error);
       });
-    
+
   })
   .controller('AppCtrl', ['$scope', 'rootScope', '$location', function AppCtrl($scope, $rootScope, $location) {
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
