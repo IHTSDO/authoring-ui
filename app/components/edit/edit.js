e9157b3d87e6525006660e29f8cf1407a0ea48ca
Quyen Ly <quocquyenit@gmail.com>
2019-10-03 14:24:04 +0700

    MAINT-897 improve sorting descritpions

--- app/components/edit/edit.js
+++ app/components/edit/edit.js
@@ -1815,7 +1815,7 @@ angular.module('singleConceptAuthoringApp.edit', [
         return;
       }
 
-      notificationService.sendMessage('Starting classification for task ' + $routeParams.taskKey, 5000);
+      notificationService.sendMessage('Starting classification for task ' + $routeParams.taskKey);
 
       if ($scope.task.status === 'New') {
         scaService.updateTask($routeParams.projectKey, $routeParams.taskKey, {'status': 'IN_PROGRESS'}).then(function (response) {
