1c8ab2fbefcdf8d2a5057eb0d433dbc888f062f5
Quyen Ly <quocquyenit@gmail.com>
2019-11-20 15:28:35 +0700

    PIP-86 Rename property

--- app/app.js
+++ app/app.js
@@ -195,7 +195,7 @@ angular
         $("#copyright_text").html(features.copyrightNotice);      
        
         if(endpoints.userGuideEndpoint) {
-          $("#user_guide").attr("href", endpoints.userGuideEndpoint)
+          $("#user_guide").attr("href", endpoints.scaUserGuideEndpoint)
         }
         
         if(endpoints.contactUsEndpoint) {
