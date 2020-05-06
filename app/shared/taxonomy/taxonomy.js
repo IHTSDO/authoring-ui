a440f5e627afe7fa2ce52ed750d321d95cc81d2f
Quyen Ly <quocquyenit@gmail.com>
2020-05-06 11:48:39 +0700

    MAINT-1296 Corrected Accept-Language value in taxonomy

--- app/shared/taxonomy/taxonomy.js
+++ app/shared/taxonomy/taxonomy.js
@@ -83,7 +83,8 @@ angular.module('singleConceptAuthoringApp.taxonomyPanel', [])
               && metadataService.getCurrentModuleId() !== usModel.moduleId) {
             for (var key in dialects) {
               if (key !== usModel.dialectId) {
-                var dialect = {id: key, label: 'PT in ' + dialects[key].toUpperCase()}
+                let languages = dialects[key].split('-');
+                var dialect = {id: key, label: 'PT in ' + (languages.length > 1 ? languages[1].toUpperCase() : languages[0].toUpperCase())}
                 extensionFilter.push(dialect);
                 $scope.selectedLanguage = dialect; // Set PT in extension by default
               }
@@ -93,17 +94,12 @@ angular.module('singleConceptAuthoringApp.taxonomyPanel', [])
             // us module
             $scope.languages = internatinalFilter;
             $scope.selectedLanguage = usPT; // Set PT in US by default
-          } else if (metadataService.getCurrentModuleId() === noModel.moduleId) {
-            // no module           
-            let noPT = {id: noModel.dialectId, label: 'PT in NO'};
-            extensionFilter.push(noPT);
-            $scope.languages = extensionFilter;
-            $scope.selectedLanguage = noPT; // Set PT in NO by default
           } else {
             // multiple dialects
             for (var key in dialects) {
               if (key !== usModel.dialectId) {
-                var dialect = {id: key, label: 'PT in ' + dialects[key].toUpperCase()}
+                let languages = dialects[key].split('-');
+                var dialect = {id: key, label: 'PT in ' + (languages.length > 1 ? languages[1].toUpperCase() : languages[0].toUpperCase())}
                 extensionFilter.push(dialect);               
               }
             }
