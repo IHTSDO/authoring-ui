8a45fbcc0f5b78d02133445575ce5133e7bce6b7
Chris Swires <csw@ihtsdo.org>
2021-05-27 11:47:29 +0100

    FRI-50 - config display

--- app/shared/sacconfig/sacconfig.js
+++ app/shared/sacconfig/sacconfig.js
@@ -1,16 +1,24 @@
 'use strict';
 
 angular.module('singleConceptAuthoringApp')
-  .controller('sacconfigCtrl', function ($scope, $rootScope, $modalInstance, scaService, metadataService, terminologyServerService, task, $location, accountService, modalService) {
+  .controller('sacconfigCtrl', function ($scope, $rootScope, $modalInstance, scaService, metadataService, terminologyServerService, task, criteria, $location, accountService, modalService) {
 
     // scope variables
     $scope.projects = null;
     $scope.task = task;
+    $scope.criteria = criteria;
     $scope.criteriaConfig = [];
 
     function initialize() {
       terminologyServerService.getSAC().then(function (response) {
-          $scope.criteriaConfig = response;
+          angular.forEach(response.content, function (configItem) {
+              angular.forEach($scope.criteria, function (selectedItem) {
+                  if(configItem.id === selectedItem.id){
+                      configItem.selected = true;
+                  }
+              });
+          });
+          $scope.criteriaConfig = response.content;
       });
       
     }
