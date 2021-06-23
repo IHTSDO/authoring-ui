611480172988a6384f7544f543f598906a35c0ef
Quyen Ly <quocquyenit@gmail.com>
2021-06-23 11:56:13 +0700

    FRI-113 Updated banner if validation report has gone stale

--- app/shared/validation/validation.js
+++ app/shared/validation/validation.js
@@ -92,6 +92,11 @@ angular.module('singleConceptAuthoringApp')
               return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
             });
 
+            console.log(status);
+            if (status && status === 'Stale') {
+              return status;
+            }
+
             if (scope.validationContainer.report && scope.validationContainer.report.rvfValidationResult) {
 
               // get the end time if specified
