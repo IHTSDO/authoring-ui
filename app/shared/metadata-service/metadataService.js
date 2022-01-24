bc81fe43c74c7f06db67a57cd6608ce2b115b912
Quyen Ly <quocquyenit@gmail.com>
2022-01-24 14:45:09 +0700

    MAINT-1860 Update to inactivation enhancements

--- app/shared/metadata-service/metadataService.js
+++ app/shared/metadata-service/metadataService.js
@@ -106,12 +106,12 @@ angular.module('singleConceptAuthoringApp')
 
     // component inactivation metadata
     var conceptInactivationReasons = [
-      {id: 'AMBIGUOUS', text: 'Ambiguous component', display: [4]},
+      {id: 'AMBIGUOUS', text: 'Ambiguous', display: [4]},
       {id: 'MOVED_ELSEWHERE', text: 'Component moved elsewhere', display: [12], absenceHistoricalAssociationSupport: true},
-      {id: 'DUPLICATE', text: 'Duplicate component', display: [7]},
-      {id: 'ERRONEOUS', text: 'Erroneous component', display: [6]},
+      {id: 'DUPLICATE', text: 'Duplicate', display: [7]},
+      {id: 'ERRONEOUS', text: 'Erroneous', display: [6]},
       //{id: 'LIMITED', text: 'Limited component', display: [9]},
-      {id: 'OUTDATED', text: 'Outdated component', display: [6,11]},
+      {id: 'OUTDATED', text: 'Outdated', display: [6,11]},
       {id: 'NONCONFORMANCE_TO_EDITORIAL_POLICY', text: 'Non-conformance to editorial policy', display: [6,12]},
       //{id: 'RETIRED', text: 'Reason not stated', display: [6, 9]}
       {id: 'CLASSIFICATION_DERIVED_COMPONENT', text: 'Classification derived component', display: [6,10]},
