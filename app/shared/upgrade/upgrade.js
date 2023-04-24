7323a01ac88ab95b7b78ca91531ef56ab8c0fc6f
Quyen Ly <quocquyenit@gmail.com>
2023-04-24 10:33:36 +0700

    MAINT-2166 Allow project lead to do the upgrade

--- app/shared/upgrade/upgrade.js
+++ app/shared/upgrade/upgrade.js
@@ -97,8 +97,8 @@ angular.module('singleConceptAuthoringApp')
       $scope.requireEnGBImport = $location.search().projectKey && $location.search().projectKey.trim() !== '';
       notificationService.clear();
       terminologyServerService.getCodeSystem($routeParams.codeSystem).then(function (codeSystem) {
-        if (codeSystem.userRoles && !codeSystem.userRoles.includes('ADMIN')) {
-          notificationService.sendError('You must login as ADMIN to upgrade this code system.');
+        if (codeSystem.userRoles && !codeSystem.userRoles.includes('ADMIN') && !codeSystem.userRoles.includes('PROJECT_LEAD')) {
+          notificationService.sendError('You must login as ADMIN or PROJECT LEAD to upgrade this code system.');
           return;
         }
 
