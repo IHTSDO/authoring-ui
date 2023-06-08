2468df3dd4084669701f3541e53367a6b87094b0
Quyen Ly <quocquyenit@gmail.com>
2023-06-08 12:40:53 +0700

    MAINT-2213 Extend whitelist popup

--- app/shared/validation/whitelistItemModal.js
+++ app/shared/validation/whitelistItemModal.js
@@ -1,23 +1,43 @@
 'use strict';
 // jshint ignore: start
 angular.module('singleConceptAuthoringApp')
-  .controller('whitelistItemModalCtrl', function ($scope, $modalInstance, failure) {
+  .controller('whitelistItemModalCtrl', function ($scope, $modalInstance, $filter, failures) {
 
     $scope.exceptionType = 'permanent';
     $scope.reason = '';
-    $scope.failure = failure;
-    
+    $scope.failures = failures;
+    $scope.expirationDate = new Date();
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    $scope.openDatePicker = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.datePickerOpened = true;
+    };
+
     $scope.cancel = function () {
       $modalInstance.dismiss();
     };
 
     $scope.confirm = function(){
-      $scope.failure.temporary = $scope.exceptionType === 'temporary';
-      if ($scope.reason.length !== 0) {
-        $scope.failure.reason = $scope.reason.trim();
+      let isTemporaryException = $scope.exceptionType === 'temporary';
+      let reason = $scope.reason.length !== 0 ? $scope.reason.trim() : null;
+      let expirationDate = null;
+      if (isTemporaryException) {
+        expirationDate =  $filter('date')($scope.expirationDate, "yyyy-MM-dd");
       }
 
-      $modalInstance.close($scope.failure);
+      angular.forEach(failures, function(item) {
+        item.temporary = isTemporaryException;
+        item.reason = reason;
+        item.expirationDate = expirationDate;
+      });
+
+      $modalInstance.close($scope.failures);
     }
   })
 ;
