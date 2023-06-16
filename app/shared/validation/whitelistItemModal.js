d57414cf773f81c9172ea0df87f9031411b0df93
Quyen Ly <quocquyenit@gmail.com>
2023-06-16 17:04:41 +0700

    MAINT-2212 Removed the expiration date from UI

--- app/shared/validation/whitelistItemModal.js
+++ app/shared/validation/whitelistItemModal.js
@@ -8,15 +8,11 @@ angular.module('singleConceptAuthoringApp')
     if ($scope.mode === 'add') {
       $scope.exceptionType = 'permanent';
       $scope.reason = '';
-      $scope.expirationDate = new Date();
+
     } else {
       $scope.exceptionType = $scope.failures[0].temporary ? 'temporary' : 'permanent';
       $scope.reason = $scope.failures[0].reason;
-      if ($scope.failures[0].temporary) {
-        $scope.expirationDate = $scope.failures[0].expirationDate;
-      }     
     }
-    
 
     $scope.dateOptions = {
       formatYear: 'yy',
@@ -29,13 +25,6 @@ angular.module('singleConceptAuthoringApp')
       $scope.datePickerOpened = true;
     };
 
-    $scope.selectExceptionType = function(type) {
-      if (type === 'temporary' && !$scope.expirationDate) {
-        $scope.expirationDate = new Date();
-      } else if (type === 'permanent') {
-        $scope.expirationDate = null;
-      }
-    };
 
     $scope.cancel = function () {
       $modalInstance.dismiss();
@@ -44,15 +33,10 @@ angular.module('singleConceptAuthoringApp')
     $scope.confirm = function(){
       let isTemporaryException = $scope.exceptionType === 'temporary';
       let reason = $scope.reason && $scope.reason.length !== 0 ? $scope.reason.trim() : null;
-      let expirationDate = null;
-      if (isTemporaryException) {
-        expirationDate =  $filter('date')($scope.expirationDate, "yyyy-MM-dd");
-      }
 
       angular.forEach(failures, function(item) {
         item.temporary = isTemporaryException;
         item.reason = reason;
-        item.expirationDate = expirationDate;
       });
 
       $modalInstance.close($scope.failures);
