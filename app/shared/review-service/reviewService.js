f5172a2a9c1a022634b4a9d7a7e75c44e6f5a463
Quyen Ly <quocquyenit@gmail.com>
2026-08-17 11:48:31 +0700

    MAINT-3127 Corrected the component subtype comparison for inferred relationships in the Review Service

--- app/shared/review-service/reviewService.js
+++ app/shared/review-service/reviewService.js
@@ -355,8 +355,8 @@ angular.module('singleConceptAuthoringApp')
               if (review.concepts.filter(function (obj) {
                   return obj.conceptId === concept.conceptId.toString();
                 }).length === 0 && concept.componentChanges.filter(function (obj) {
-                  return obj.componentSubType !== 'INFERRED_RELATIONSHIP';
-                }).length !== 0) {
+                  return obj.componentSubType !== '900000000000011006';
+                }).length !== 0) {  
 
                 concept.conceptId = concept.conceptId.toString();
                 concept.lastUpdatedTime = change.commitDate;
@@ -366,14 +366,14 @@ angular.module('singleConceptAuthoringApp')
               else if (review.conceptsClassified.filter(function (obj) {
                   return obj.conceptId === concept.conceptId.toString();
                 }).length === 0 && concept.componentChanges.filter(function (obj) {
-                  return obj.componentSubType === 'INFERRED_RELATIONSHIP';
+                  return obj.componentSubType === '900000000000011006';
                 }).length !== 0) {
                 concept.conceptId = concept.conceptId.toString();
                 concept.lastUpdatedTime = change.commitDate;
                 review.conceptsClassified.push(concept);
               }
               else if (concept.componentChanges.filter(function (obj) {
-                    return obj.componentSubType !== 'INFERRED_RELATIONSHIP';
+                    return obj.componentSubType !== '900000000000011006';
                   }).length !== 0) {
                 var updateConcept = review.concepts.filter(function (obj) {
                   return obj.conceptId === concept.conceptId.toString();
